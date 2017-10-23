841c7e925964ec87b7738678cf2f331626c32872
fizdalf <fizdalf@gmail.com>
2017-10-23 12:01:02 +0100

    - Closes issue #54

--- htdocs/DailyReport.php
+++ htdocs/DailyReport.php
@@ -1,34 +1,36 @@
 <?php
 require_once("config.inc.php");
-require_once($cfg["path_bu"]."/BUDailyReport.inc.php");
+require_once($cfg["path_bu"] . "/BUDailyReport.inc.php");
 
-$buDailyReport= new BUDailyReport( $this );
+$buDailyReport = new BUDailyReport($this);
 
-if ( $_REQUEST['daysAgo'] ){
-  $daysAgo = $_REQUEST['daysAgo'];
+if ($_REQUEST['daysAgo']) {
+    $daysAgo = $_REQUEST['daysAgo'];
+} else {
+    $daysAgo = 1;
 }
-else{
-  $daysAgo = 1;
-}
-  
-switch ( $_REQUEST['action'] ) {
-  
-  case 'fixedIncidents' :
-    $buDailyReport->fixedIncidents( $daysAgo );
-    break;
-  case 'focActivities' :
-    $buDailyReport->focActivities( $daysAgo );
-    break;
-  case 'prepayOverValue' :
-    $buDailyReport->prepayOverValue( $daysAgo );
-    break;
-  case 'outstandingIncidents' :
-    $buDailyReport->outstandingIncidents( $daysAgo );
-    break;
-  case 'outstandingPriorityFiveIncidents' :
-    $buDailyReport->outstandingIncidents( $daysAgo, true );
-    break;
-  default :
-    break;
+
+switch ($_REQUEST['action']) {
+
+    case 'fixedIncidents' :
+        $buDailyReport->fixedIncidents($daysAgo);
+        break;
+    case 'focActivities' :
+        $buDailyReport->focActivities($daysAgo);
+        break;
+    case 'prepayOverValue' :
+        $buDailyReport->prepayOverValue($daysAgo);
+        break;
+    case 'outstandingIncidents' :
+        $buDailyReport->outstandingIncidents($daysAgo);
+        break;
+    case 'outstandingPriorityFiveIncidents' :
+        $buDailyReport->outstandingIncidents($daysAgo, true);
+        break;
+    case 'p5SRWithoutSalesOrders':
+        $buDailyReport->p5IncidentsWithoutSalesOrders($daysAgo);
+        break;
+    default :
+        break;
 }
 ?>
