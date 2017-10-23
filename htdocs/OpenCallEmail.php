f97bd720c14d9928e021693e86e3f819a08999d2
fizdalf <fizdalf@gmail.com>
2017-10-23 14:12:54 +0100

    - Closes issue #115

--- htdocs/OpenCallEmail.php
+++ htdocs/OpenCallEmail.php
@@ -17,8 +17,6 @@ define('FORMAT_MYSQL_UK_DATE', '%e/%c/%Y');
 
 
 $domain = 'cnc-ltd.co.uk';
-//if ($GLOBALS['server_type'] == MAIN_CONFIG_SERVER_TYPE_LIVE) {
-//}
 
 if (!$localDb = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD)) {
     echo 'Could not connect to mysql host ' . DB_HOST;
@@ -29,6 +27,8 @@ if (!$localDb = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD)) {
 get list of engineers with open calls
 */
 $localDb->select_db(DB_NAME);
+
+$outputEmails = isset($_GET['outputEmails']);
 
 $query =
     'SELECT
@@ -45,6 +45,7 @@ $query =
 
 $resultSet = $localDb->query($query);
 
+//this finds all the engineers that have open activities
 /*
 Send each engineer an email
 */
@@ -52,9 +53,10 @@ $engineers = $resultSet->fetch_all(MYSQLI_ASSOC);
 
 $buMail = new BUMail($this);
 
+$managers = [];
 foreach ($engineers as $row) {
     $query =
-        'SELECT caa_callactivityno, cus_name, CONCAT(firstName, " ", lastName) as name, caa_date' .
+        'SELECT caa_callactivityno, cus_name, CONCAT(firstName, " ", lastName) as name, caa_date, cns_manager ' .
         ' FROM callactivity ' .
         ' JOIN problem ON pro_problemno = caa_problemno' .
         ' JOIN callacttype ON cat_callacttypeno = caa_callacttypeno' .
@@ -71,7 +73,9 @@ foreach ($engineers as $row) {
     $headers = "From: " . $sender_name . " <" . $sender_email . ">\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html";
-    ob_start()
+    if (!$outputEmails) {
+        ob_start();
+    }
     ?>
     <HTML>
     <P>
@@ -91,6 +95,24 @@ foreach ($engineers as $row) {
         </TR>
         <?php
         while ($i = $result->fetch_row()) {
+            $managerId = $i[4];
+            if ($managerId && (strtotime($i[3]) <= strtotime('-5 days', time()))) {
+                //this guy has a manager and this activity was open more than two days ago
+                if (!isset($managers[$managerId])) {
+                    $managers[$managerId] = (object)[
+                        "minionConsultants" => []
+                    ];
+                }
+
+                if (!isset($managers[$managerId]->minionConsultants[$row['cns_consno']])) {
+                    $managers[$managerId]->minionConsultants[$row['cns_consno']] = (object)[
+                        "name" => $row['Engineer'],
+                        "openActivities" => []
+                    ];
+                }
+
+                $managers[$managerId]->minionConsultants[$row['cns_consno']]->openActivities[] = $i;
+            }
             ?>
             <TR>
                 <TD>
@@ -109,36 +131,157 @@ foreach ($engineers as $row) {
     </TABLE>
     </HTML>
     <?php
-    $body = ob_get_contents();
-    ob_end_clean();
+
+    if (!$outputEmails) {
+        $body = ob_get_contents();
+        ob_end_clean();
 
 
-    $emailSubject = "You have open requests";
+        $emailSubject = "You have open requests";
 
-    $buMail->mime->setHTMLBody($body);
+        $buMail->mime->setHTMLBody($body);
 
-    $body = $buMail->mime->get();
+        $body = $buMail->mime->get();
 
-    $hdrs = array(
-        'From' => CONFIG_SALES_MANAGER_EMAIL,
-        'Subject' => $emailSubject
-    );
+        $hdrs = array(
+            'From' => CONFIG_SALES_MANAGER_EMAIL,
+            'Subject' => $emailSubject
+        );
 
-    $hdrs = $buMail->mime->headers($hdrs);
+        $hdrs = $buMail->mime->headers($hdrs);
 
-    $sendTo = $row['cns_logname'] . '@' . $domain;
+        $sendTo = $row['cns_logname'] . '@' . $domain;
 
-    $sent = $buMail->send(
-        $sendTo,
-        $hdrs,
-        $body,
-        true
-    );
+        $sent = $buMail->send(
+            $sendTo,
+            $hdrs,
+            $body
+        );
 
-    if ($sent) {
-        echo 'email sent to ' . $row['Engineer'] . ' email: ' . $sendTo;
-    } else {
-        echo 'failed to send email to ' . $row['Engineer'] . ' email: ' . $sendTo;
+        if ($sent) {
+            echo 'email sent to ' . $row['Engineer'] . ' email: ' . $sendTo;
+            echo '<br>';
+        } else {
+            echo 'failed to send email to ' . $row['Engineer'] . ' email: ' . $sendTo;
+            echo '<br>';
+        }
     }
+}
+
+foreach ($managers as $managerId => $manager) {
+    //get information about the manager
+
+    $managersRows = $localDb->query('SELECT cns_logname FROM consultant WHERE cns_consno = ' . $managerId);
+
+    $managerRow = $managersRows->fetch_assoc();
+
+    if (!$managerRow) {
+        echo "<br>Manager with id $managerId not found, skipping<br>";
+        continue;
+    }
+
+    $sender_name = "Call System";
+    $sender_email = OS_CALL_EMAIL_FROM_USER;
+    $headers = "From: " . $sender_name . " <" . $sender_email . ">\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html";
+    if (!$outputEmails) {
+        ob_start();
+    }
+    ?>
+    <HTML>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+        }
+    </style>
+    <P>
+        The following consultants under your management have open activities for 2 days or more
+    </P>
+    <TABLE>
+        <TR>
+            <td>
+                Consultant Name
+            </td>
+            <TD>
+                Ref
+            </TD>
+            <TD>
+                Customer
+            </TD>
+            <TD>
+                Date
+            </TD>
+        </TR>
+        <?php
+        foreach ($manager->minionConsultants as $minionConsultant) {
+
+            $isFirst = true;
+
+            foreach ($minionConsultant->openActivities as $openActivity) {
+
+                ?>
+                <TR>
+                    <?php
+                    if ($isFirst) { ?>
+                        <TD rowspan="<?= count($minionConsultant->openActivities); ?>">
+                            <?= $minionConsultant->name ?>
+                        </TD>
+                        <?php
+                    }
+                    ?>
+                    <TD>
+                        <A href="http://<?= $_SERVER['HTTP_HOST'] ?>/Activity.php?action=displayActivity&callActivityID=<?= $openActivity[0] ?>"><?= $openActivity[0] ?></A>
+                    </TD>
+
+                    <TD>
+                        <?= $openActivity[1] ?>
+                    </TD>
+                    <TD>
+                        <?= $openActivity[3] ?>
+                    </TD>
+                </TR>
+                <?php
+                if ($isFirst) {
+                    $isFirst = false;
+                }
+            }
+        }
+        ?>
+    </TABLE>
+    </HTML>
+    <?php
+    if (!$outputEmails) {
+        $body = ob_get_contents();
+        ob_end_clean();
+
+        $emailSubject = "Your managed engineers have open requests";
+
+        $buMail->mime->setHTMLBody($body);
+
+        $body = $buMail->mime->get();
+
+        $hdrs = array(
+            'From' => CONFIG_SALES_MANAGER_EMAIL,
+            'Subject' => $emailSubject
+        );
+
+        $hdrs = $buMail->mime->headers($hdrs);
+
+        $sendTo = $managerRow['cns_logname'] . '@' . $domain;
+
+        $sent = $buMail->send(
+            $sendTo,
+            $hdrs,
+            $body
+        );
+
+        if ($sent) {
+            echo 'email sent to ' . $managerRow['cns_logname'] . ' email: ' . $sendTo;
+        } else {
+            echo 'failed to send email to ' . $managerRow['cns_logname'] . ' email: ' . $sendTo;
+        }
+    }
+
 }
 ?>
