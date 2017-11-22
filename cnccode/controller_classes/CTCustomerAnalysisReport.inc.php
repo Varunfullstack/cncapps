f60ecf3dfeb592a7f8c8daff37c60ff2ac1f5a9a
fizdalf <fizdalf@gmail.com>
2017-11-22 13:19:02 +0000

    - Sorted issue out.

--- cnccode/controller_classes/CTCustomerAnalysisReport.inc.php
+++ cnccode/controller_classes/CTCustomerAnalysisReport.inc.php
@@ -58,7 +58,8 @@ class CTCustomerAnalysisReport extends CTCNC
 
                     $template->set_block('page', 'contractsBlock', 'contracts');
 
-                    foreach ($results as $row) {
+                    foreach ($results as $contractName => $row) {
+
                         $template->set_var(
                             array(
                                 'contract' => $contractName,
@@ -80,24 +81,46 @@ class CTCustomerAnalysisReport extends CTCNC
                     exit;
                 } else { // Screen Report
 
-                    $this->template->set_block('CustomerAnalysisReport', 'contractsBlock', 'contracts');
+
                     $totalSales = 0;
                     $totalCost = 0;
                     $totalLabour = 0;
                     $totalLabourHours = 0;
+                    $grandTotalSales = 0;
+                    $grandTotalCost = 0;
+                    $grandTotalLabour = 0;
+                    $grandTotalLabourHours = 0;
 
                     if (isset($_REQUEST['orderBy'])) {
+                        $columns = [
+                            "sales" => [],
+                            "cost" => [],
+                            "labourCost" => [],
+                            "labourHours" => [],
+                            "profitPercent" => [],
+                            "profit" => [],
+                        ];
                         foreach ($results as $key => $row) {
-                            $sales[$key] = $row['sales'];
-                            $cost[$key] = $row['cost'];
-                            $labourCost[$key] = $row['labourCost'];
-                            $labourHours[$key] = $row['labourHours'];
-                            $profitPercent[$key] = $row['profitPercent'];
-                            $profit[$key] = $row['profit'];
+                            $columns['sales'][$key] = $row['sales'];
+                            $columns['cost'][$key] = $row['cost'];
+                            $columns['labourCost'][$key] = $row['labourCost'];
+                            $columns['labourHours'][$key] = $row['labourHours'];
+                            $columns['profitPercent'][$key] = $row['profitPercent'];
+                            $columns['profit'][$key] = $row['profit'];
                         }
-                        array_multisort($$_REQUEST['orderBy'], SORT_ASC, $results);
+                        array_multisort($columns[$_REQUEST['orderBy']], SORT_ASC, $results);
                     }
 
+                    $this->template->set_block('CustomerAnalysisReport', 'contractsBlock', 'contracts');
+                    $reportUrl =
+                        $this->buildLink(
+                            'CustomerAnalysisReport.php',
+                            array(
+                                'searchForm[1][customerID]' => $_REQUEST ['searchForm'][1]['customerID'],
+                                'searchForm[1][startYearMonth]' => $_REQUEST ['searchForm'][1]['startYearMonth'],
+                                'searchForm[1][endYearMonth]' => $_REQUEST ['searchForm'][1]['endYearMonth'],
+                            )
+                        );
                     foreach ($results as $contractName => $row) {
 
                         if ($row['profit'] <= 0) {
@@ -106,15 +129,25 @@ class CTCustomerAnalysisReport extends CTCNC
                             $profitAlertClass = '';
                         }
 
-                        $reportUrl =
-                            $this->buildLink(
-                                'CustomerAnalysisReport.php',
-                                array(
-                                    'searchForm[1][customerID]' => $_REQUEST ['searchForm'][1]['customerID'],
-                                    'searchForm[1][startYearMonth]' => $_REQUEST ['searchForm'][1]['startYearMonth'],
-                                    'searchForm[1][endYearMonth]' => $_REQUEST ['searchForm'][1]['endYearMonth'],
-                                )
-                            );
+                        $target = 'contracts';
+                        $handle = 'contractsBlock';
+
+                        if ($contractName == 'Other Sales') {
+                            $this->template->setBlock('CustomerAnalysisReport', 'otherSalesBlock', 'otherSales');
+                            $target = 'otherSales';
+                            $handle = 'otherSalesBlock';
+
+                        } else {
+                            $totalSales += $row['sales'];
+                            $totalCost += $row['cost'];
+                            $totalLabour += $row['labourCost'];
+                            $totalLabourHours += $row['labourHours'];
+                        }
+
+                        $grandTotalSales += $row['sales'];
+                        $grandTotalCost += $row['cost'];
+                        $grandTotalLabour += $row['labourCost'];
+                        $grandTotalLabourHours += $row['labourHours'];
 
                         $this->template->set_var(
                             array(
@@ -124,17 +157,14 @@ class CTCustomerAnalysisReport extends CTCNC
                                 'labour' => number_format($row['labourCost'], 2),
                                 'profit' => number_format($row['profit'], 2),
                                 'profitPercent' => $row['profitPercent'],
-                                'labourHours' => $row['labourHours'],
+                                'labourHours' => number_format($row['labourHours'], 0),
                                 'profitAlertClass' => $profitAlertClass,
                                 'reportUrl' => $reportUrl
                             )
                         );
-                        $this->template->parse('contracts', 'contractsBlock', true);
+                        $this->template->parse($target, $handle, true);
 
-                        $totalSales += $row['sales'];
-                        $totalCost += $row['cost'];
-                        $totalLabour += $row['labourCost'];
-                        $totalLabourHours += $row['labourHours'];
+
                     }
                     $this->template->set_var(
                         array(
@@ -142,11 +172,20 @@ class CTCustomerAnalysisReport extends CTCNC
                             'totalCost' => number_format($totalCost, 2),
                             'totalLabour' => number_format($totalLabour, 2),
                             'totalProfit' => number_format($totalSales - $totalCost - $totalLabour, 2),
-                            'totalProfitPercent' => number_format(100 - (($totalCost + $totalLabour) / $totalSales) * 100, 2),
+                            'totalProfitPercent' => number_format($totalSales > 0 ? 100 - (($totalCost + $totalLabour) / $totalSales) * 100 : 0, 2),
                             'totalLabourHours' => number_format($totalLabourHours, 2),
                         )
                     );
-
+                    $this->template->set_var(
+                        array(
+                            'grandTotalSales' => number_format($grandTotalSales, 2),
+                            'grandTotalCost' => number_format($grandTotalCost, 2),
+                            'grandTotalLabour' => number_format($grandTotalLabour, 2),
+                            'grandTotalProfit' => number_format($grandTotalSales - $grandTotalCost - $grandTotalLabour, 2),
+                            'grandTotalProfitPercent' => number_format($grandTotalSales > 0 ? 100 - (($grandTotalCost + $grandTotalLabour) / $grandTotalSales) * 100 : 0, 2),
+                            'grandTotalLabourHours' => number_format($grandTotalLabourHours, 2),
+                        )
+                    );
 
                 }
 
