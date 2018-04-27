7671f3ddff89b003d07c1d9cdf57bbfd6cc919cf
fizdalf <fizdalf@gmail.com>
2018-04-27 09:14:01 +0100

    Create changelog file

--- cnccode/data_classes/DBECallActType.inc.php
+++ cnccode/data_classes/DBECallActType.inc.php
@@ -7,6 +7,31 @@ require_once($cfg["path_gc"] . "/DBEntity.inc.php");
 
 class DBECallActType extends DBEntity
 {
+    const callActTypeID = "callActTypeID";
+    const description = "description";
+    const oohMultiplier = "oohMultiplier";
+    const itemID = "itemID";
+    const maxHours = "maxHours";
+    const minHours = "minHours";
+    const customerEmailFlag = "customerEmailFlag";
+    const requireCheckFlag = "requireCheckFlag";
+    const allowExpensesFlag = "allowExpensesFlag";
+    const allowReasonFlag = "allowReasonFlag";
+    const allowActionFlag = "allowActionFlag";
+    const allowFinalStatusFlag = "allowFinalStatusFlag";
+    const reqReasonFlag = "reqReasonFlag";
+    const reqActionFlag = "reqActionFlag";
+    const reqFinalStatusFlag = "reqFinalStatusFlag";
+    const allowSCRFlag = "allowSCRFlag";
+    const curValueFlag = "curValueFlag";
+    const travelFlag = "travelFlag";
+    const activeFlag = "activeFlag";
+    const showNotChargeableFlag = "showNotChargeableFlag";
+    const engineerOvertimeFlag = "engineerOvertimeFlag";
+    const onSiteFlag = "onSiteFlag";
+    const portalDisplayFlag = "portalDisplayFlag";
+    const visibleInSRFlag = "visibleInSRFlag";
+
     /**
      * calls constructor()
      * @access public
@@ -18,29 +43,46 @@ class DBECallActType extends DBEntity
     {
         parent::__construct($owner);
         $this->setTableName("callacttype");
-        $this->addColumn("callActTypeID", DA_ID, DA_NOT_NULL, "cat_callacttypeno");
-        $this->addColumn("description", DA_STRING, DA_NOT_NULL, "cat_desc");
-        $this->addColumn("oohMultiplier", DA_FLOAT, DA_ALLOW_NULL, "cat_ooh_multiplier");
-        $this->addColumn("itemID", DA_INTEGER, DA_ALLOW_NULL, "cat_itemno");
-        $this->addColumn("maxHours", DA_FLOAT, DA_ALLOW_NULL, "cat_max_hours");
-        $this->addColumn("minHours", DA_FLOAT, DA_ALLOW_NULL, "cat_min_hours");
-        $this->addColumn("customerEmailFlag", DA_YN, DA_NOT_NULL);// send emails to customers
-        $this->addColumn("requireCheckFlag", DA_YN, DA_NOT_NULL, "cat_req_check_flag");        // rquires checking before sales order
-        $this->addColumn("allowExpensesFlag", DA_YN, DA_NOT_NULL, "cat_allow_exp_flag");    // allow expenses against this type
-        $this->addColumn("allowReasonFlag", DA_YN, DA_NOT_NULL, "cat_problem_flag");                // allow problem notes
-        $this->addColumn("allowActionFlag", DA_YN, DA_NOT_NULL, "cat_action_flag");
-        $this->addColumn("allowFinalStatusFlag", DA_YN, DA_NOT_NULL, "cat_resolve_flag");
-        $this->addColumn("reqReasonFlag", DA_YN, DA_NOT_NULL, "cat_r_problem_flag");    // whether these notepads are required
-        $this->addColumn("reqActionFlag", DA_YN, DA_NOT_NULL, "cat_r_action_flag");
-        $this->addColumn("reqFinalStatusFlag", DA_YN, DA_NOT_NULL, "cat_r_resolve_flag");
-        $this->addColumn("allowSCRFlag", DA_YN, DA_NOT_NULL);
-        $this->addColumn("curValueFlag", DA_YN, DA_NOT_NULL);                                                        // is this activity type a currency value
-        $this->addColumn("travelFlag", DA_YN, DA_NOT_NULL);            // is this a travel activity?
-        $this->addColumn("activeFlag", DA_YN, DA_NOT_NULL);            // is	this an active activity?
-        $this->addColumn("showNotChargeableFlag", DA_YN, DA_NOT_NULL);            // show charagable text on activity emails?
-        $this->addColumn("engineerOvertimeFlag", DA_YN, DA_NOT_NULL);            // Allow engineer overtime
-        $this->addColumn("onSiteFlag", DA_YN, DA_NOT_NULL, "cat_on_site_flag");
-        $this->addColumn("portalDisplayFlag", DA_YN, DA_NOT_NULL, "cat_portal_display_flag");
+        $this->addColumn(self::callActTypeID, DA_ID, DA_NOT_NULL, "cat_callacttypeno");
+        $this->addColumn(self::description, DA_STRING, DA_NOT_NULL, "cat_desc");
+        $this->addColumn(self::oohMultiplier, DA_FLOAT, DA_ALLOW_NULL, "cat_ooh_multiplier");
+        $this->addColumn(self::itemID, DA_INTEGER, DA_ALLOW_NULL, "cat_itemno");
+        $this->addColumn(self::maxHours, DA_FLOAT, DA_ALLOW_NULL, "cat_max_hours");
+        $this->addColumn(self::minHours, DA_FLOAT, DA_ALLOW_NULL, "cat_min_hours");
+        $this->addColumn(self::customerEmailFlag, DA_YN, DA_NOT_NULL);// send emails to customers
+        $this->addColumn(self::requireCheckFlag,
+                         DA_YN,
+                         DA_NOT_NULL,
+                         "cat_req_check_flag");        // rquires checking before sales order
+        $this->addColumn(self::allowExpensesFlag,
+                         DA_YN,
+                         DA_NOT_NULL,
+                         "cat_allow_exp_flag");    // allow expenses against this type
+        $this->addColumn(self::allowReasonFlag,
+                         DA_YN,
+                         DA_NOT_NULL,
+                         "cat_problem_flag");                // allow problem notes
+        $this->addColumn(self::allowActionFlag, DA_YN, DA_NOT_NULL, "cat_action_flag");
+        $this->addColumn(self::allowFinalStatusFlag, DA_YN, DA_NOT_NULL, "cat_resolve_flag");
+        $this->addColumn(self::reqReasonFlag,
+                         DA_YN,
+                         DA_NOT_NULL,
+                         "cat_r_problem_flag");    // whether these notepads are required
+        $this->addColumn(self::reqActionFlag, DA_YN, DA_NOT_NULL, "cat_r_action_flag");
+        $this->addColumn(self::reqFinalStatusFlag, DA_YN, DA_NOT_NULL, "cat_r_resolve_flag");
+        $this->addColumn(self::allowSCRFlag, DA_YN, DA_NOT_NULL);
+        $this->addColumn(self::curValueFlag,
+                         DA_YN,
+                         DA_NOT_NULL);                                                        // is this activity type a currency value
+        $this->addColumn(self::travelFlag, DA_YN, DA_NOT_NULL);            // is this a travel activity?
+        $this->addColumn(self::activeFlag, DA_YN, DA_NOT_NULL);            // is	this an active activity?
+        $this->addColumn(self::showNotChargeableFlag,
+                         DA_YN,
+                         DA_NOT_NULL);            // show charagable text on activity emails?
+        $this->addColumn(self::engineerOvertimeFlag, DA_YN, DA_NOT_NULL);            // Allow engineer overtime
+        $this->addColumn(self::onSiteFlag, DA_YN, DA_NOT_NULL, "cat_on_site_flag");
+        $this->addColumn(self::portalDisplayFlag, DA_YN, DA_NOT_NULL, "cat_portal_display_flag");
+        $this->addColumn(self::visibleInSRFlag, DA_YN, DA_NOT_NULL, 'cat_visible_in_sr_flag');
         $this->setAddColumnsOff();
         $this->setPK(0);
     }
