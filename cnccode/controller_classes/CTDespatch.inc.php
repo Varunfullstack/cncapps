8acba1427431ed646f9e49204e275711aeb70dd2
fizdalf <fizdalf@gmail.com>
2018-05-16 10:05:31 +0100

    Update permissions

--- cnccode/controller_classes/CTDespatch.inc.php
+++ cnccode/controller_classes/CTDespatch.inc.php
@@ -46,6 +46,9 @@ class CTDespatch extends CTCNC
     function __construct($requestMethod, $postVars, $getVars, $cookieVars, $cfg)
     {
         parent::__construct($requestMethod, $postVars, $getVars, $cookieVars, $cfg);
+        $roles = [
+            'sales'
+        ];
         if (!self::hasPermissions($roles)) {
             Header("Location: /NotAllowed.php");
             exit;
