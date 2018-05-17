bcffd47b57f19ec6f4d8c6f88e480f7be6d42a1a
fizdalf <fizdalf@gmail.com>
2018-05-17 10:19:26 +0100

    do not try to output the content as it's not interesting

--- htdocs/call_url.php
+++ htdocs/call_url.php
@@ -89,17 +89,6 @@ if ($result['http_code'] != 200) {
     die('HTTP Error: ' . $result['http_code']);
 }
 
-if (preg_match("/\/(\d+)$/", $argv[1], $matches)) {
-    $name = $matches[1];
-} else {
-    $name = 'unknown';
-}
-
-$handle = fopen('c:/logs/' . $name . '-' . (new DateTime())->format('Y-m-d H:i:s') . '.html', 'w');
-fwrite($handle, print_r($result));
-fclose($handle);
-
-
 //we are going to use this to add to the monitoring db
 $dsn = 'mysql:host=localhost;dbname=cncappsdev';
 $DB_USER = "webuser";
