5e5dbcd7f8d07e5eb8714ff518cde431d82f05c6
fizdalf <fizdalf@gmail.com>
2018-06-05 10:06:30 +0100

    Fix presentation and ordering issues

--- cnccode/controller_classes/CTManagementReports.inc.php
+++ cnccode/controller_classes/CTManagementReports.inc.php
@@ -397,33 +397,33 @@ class CTManagementReports extends CTCNC
                 'customer'      => Controller::htmlDisplayText($row->customer),
                 'sector'        => Controller::htmlDisplayText($row->sector),
                 'noOfPCs'       => Controller::htmlDisplayText($row->noOfPCs),
-                'noOfServers'   => Controller::htmlDisplayText($row->noOfServers),
-                'salesMonth1'   => Controller::formatNumber($row->salesMonth1, 0),
-                'profitMonth1'  => Controller::formatNumber($row->profitMonth1, 0),
-                'salesMonth2'   => Controller::formatNumber($row->salesMonth2, 0),
-                'profitMonth2'  => Controller::formatNumber($row->profitMonth2, 0),
-                'salesMonth3'   => Controller::formatNumber($row->salesMonth3, 0),
-                'profitMonth3'  => Controller::formatNumber($row->profitMonth3, 0),
-                'salesMonth4'   => Controller::formatNumber($row->salesMonth4, 0),
-                'profitMonth4'  => Controller::formatNumber($row->profitMonth4, 0),
-                'salesMonth5'   => Controller::formatNumber($row->salesMonth5, 0),
-                'profitMonth5'  => Controller::formatNumber($row->profitMonth5, 0),
-                'salesMonth6'   => Controller::formatNumber($row->salesMonth6, 0),
-                'profitMonth6'  => Controller::formatNumber($row->profitMonth6, 0),
-                'salesMonth7'   => Controller::formatNumber($row->salesMonth7, 0),
-                'profitMonth7'  => Controller::formatNumber($row->profitMonth7, 0),
-                'salesMonth8'   => Controller::formatNumber($row->salesMonth8, 0),
-                'profitMonth8'  => Controller::formatNumber($row->profitMonth8, 0),
-                'salesMonth9'   => Controller::formatNumber($row->salesMonth9, 0),
-                'profitMonth9'  => Controller::formatNumber($row->profitMonth9, 0),
-                'salesMonth10'  => Controller::formatNumber($row->salesMonth10, 0),
-                'profitMonth10' => Controller::formatNumber($row->profitMonth10, 0),
-                'salesMonth11'  => Controller::formatNumber($row->salesMonth11, 0),
-                'profitMonth11' => Controller::formatNumber($row->profitMonth11, 0),
-                'salesMonth12'  => Controller::formatNumber($row->salesMonth12, 0),
-                'profitMonth12' => Controller::formatNumber($row->profitMonth12, 0),
-                'totalSales'    => Controller::formatNumber($row->salesTotal, 0),
-                'totalProfit'   => Controller::formatNumber($row->profitTotal, 0)
+                'noOfServers'   => $row->noOfServers,
+                'salesMonth1'   => number_format($row->salesMonth1, 2),
+                'profitMonth1'  => round($row->profitMonth1, 2),
+                'salesMonth2'   => round($row->salesMonth2, 2),
+                'profitMonth2'  => round($row->profitMonth2, 2),
+                'salesMonth3'   => round($row->salesMonth3, 2),
+                'profitMonth3'  => round($row->profitMonth3, 2),
+                'salesMonth4'   => round($row->salesMonth4, 2),
+                'profitMonth4'  => round($row->profitMonth4, 2),
+                'salesMonth5'   => round($row->salesMonth5, 2),
+                'profitMonth5'  => round($row->profitMonth5, 2),
+                'salesMonth6'   => round($row->salesMonth6, 2),
+                'profitMonth6'  => round($row->profitMonth6, 2),
+                'salesMonth7'   => round($row->salesMonth7, 2),
+                'profitMonth7'  => round($row->profitMonth7, 2),
+                'salesMonth8'   => round($row->salesMonth8, 2),
+                'profitMonth8'  => round($row->profitMonth8, 2),
+                'salesMonth9'   => round($row->salesMonth9, 2),
+                'profitMonth9'  => round($row->profitMonth9, 2),
+                'salesMonth10'  => round($row->salesMonth10, 2),
+                'profitMonth10' => round($row->profitMonth10, 2),
+                'salesMonth11'  => round($row->salesMonth11, 2),
+                'profitMonth11' => round($row->profitMonth11, 2),
+                'salesMonth12'  => round($row->salesMonth12, 2),
+                'profitMonth12' => round($row->profitMonth12, 2),
+                'totalSales'    => round($row->salesTotal, 2),
+                'totalProfit'   => round($row->profitTotal, 2),
             ];
         }
         return $data;
@@ -435,7 +435,7 @@ class CTManagementReports extends CTCNC
 
         $this->setTemplateFiles('ManagementReportsSalesCustomer', 'ManagementReportsSalesCustomer.inc');
 
-        $this->setPageTitle("Sales By Customer");
+        $this->setPageTitle("Sales By Customer Profile");
 
         // year selector
         $this->template->set_block('ManagementReportsSalesCustomer', 'yearBlock', 'years');
