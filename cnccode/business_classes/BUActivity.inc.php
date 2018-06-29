b0c102cbc22493968029eb44a4261cc057ba32e8
fizdalf <fizdalf@gmail.com>
2018-06-29 11:22:19 +0100

    Fix issue with sales orders

--- cnccode/business_classes/BUActivity.inc.php
+++ cnccode/business_classes/BUActivity.inc.php
@@ -34,7 +34,7 @@ require_once($cfg ["path_dbe"] . "/DBEUser.inc.php");
 require_once($cfg ["path_dbe"] . "/DBEJUser.inc.php");
 require_once($cfg ["path_dbe"] . "/DBESite.inc.php");
 require_once($cfg ["path_bu"] . "/BUMail.inc.php");
-require_once($cfg["path_dbe"]. "/DBEJPorhead.inc.php");
+require_once($cfg["path_dbe"] . "/DBEJPorhead.inc.php");
 
 define(
     'BUACTIVITY_RESOLVED',
@@ -3171,6 +3171,7 @@ class BUActivity extends Business
         $lastProblemID = false;
         $lastUserID = false;
         $lastDate = false;
+        $consultantName = '';
 
         while ($dbeJCallActivity->fetchNext()) {
 
@@ -3366,7 +3367,6 @@ class BUActivity extends Business
 
             $lastProblemID = $problemID;
 
-            $consultantName = '';
             if ($lastUserID != $dbeJCallActivity->getValue(
                     DBEJCallActivity::userID
                 ) or $lastDate != $dbeJCallActivity->getValue(
@@ -3414,7 +3414,6 @@ class BUActivity extends Business
             );
 
             if ($normalHours > 0) {
-
                 $description = $consultantName . ' - Consultancy';
                 $sequenceNo++;
                 $dbeOrdline->setValue(
