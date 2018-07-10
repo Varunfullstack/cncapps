0474ab522027ea03d8270f24475f651c92a2cdbd
fizdalf <fizdalf@gmail.com>
2018-07-10 11:01:17 +0100

    Update to customer response flag to N

--- cnccode/business_classes/BUActivity.inc.php
+++ cnccode/business_classes/BUActivity.inc.php
@@ -7615,7 +7615,7 @@ is currently a balance of ';
         $setEndTimeToNow = false,
         $userID,
         $moveToUsersQueue = false,
-        $awatingCustomerFlag = false
+        $resetAwaitingCustomerResponse = false
     )
     {
         $dbeCallActivity = new DBECallActivity($this);
@@ -7857,19 +7857,27 @@ is currently a balance of ';
             DBEJCallActivity::serverGuard,
             $dbeCallActivity->getValue(DBEJCallActivity::serverGuard)
         );
+
+        if ($resetAwaitingCustomerResponse) {
+
+            $dbeProblem->setValue(
+                DBEJProblem::awaitingCustomerResponseFlag,
+                'N'
+            );
+            $dbeProblem->updateRow();
+
+            $dbeCallActivity->setValue(
+                DBECallActivity::awaitingCustomerResponseFlag,
+                'N'
+            );
+        }
+
         $dbeCallActivity->insertRow();
 
         $ret = $dbeCallActivity->getPKValue();
 
         $this->highActivityAlertCheck($dbeProblem->getValue(DBEJProblem::problemID));
 
-        if ($awatingCustomerFlag) {
-            $dbeProblem->setValue(
-                DBEJProblem::awaitingCustomerResponseFlag,
-                'Y'
-            );
-            $dbeProblem->updateRow();
-        }
 
         if ($passedReason) {
             $this->updatedByAnotherUser(
