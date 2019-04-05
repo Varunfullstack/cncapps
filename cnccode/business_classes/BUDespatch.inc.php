161d5cfdfbe41c4fa562b309198dfa49b28e86b9
fizdalf <fizdalf@gmail.com>
2019-04-05 11:40:34 +0100

    Fix broken stuff

--- cnccode/business_classes/BUDespatch.inc.php
+++ cnccode/business_classes/BUDespatch.inc.php
@@ -17,11 +17,16 @@ require_once($cfg["path_dbe"] . "/DBEOrdline.inc.php");
 
 class BUDespatch extends Business
 {
-    var $dbeOrdline = '';
+
+    const sequenceNo = "sequenceNo";
+    const qtyToDespatch = "qtyToDespatch";
+    /** @var DBEOrdline */
+    public $dbeOrdline;
 
     /**
      * Constructor
      * @access Public
+     * @param $owner
      */
     function __construct(&$owner)
     {
@@ -48,11 +53,16 @@ class BUDespatch extends Business
         } else {
             $ret = $dbeJOrdhead->getDespatchRows($customerID);
         }
-        $this->getData($dbeJOrdhead, $dsResults);
+        $this->getData(
+            $dbeJOrdhead,
+            $dsResults
+        );
         return $ret;
     }
 
-    function getLinesByID($ordheadID, &$dsOrdline)
+    function getLinesByID($ordheadID,
+                          &$dsOrdline
+    )
     {
         $this->setMethodName('getLinesByID');
         $ret = FALSE;
@@ -60,9 +70,15 @@ class BUDespatch extends Business
             $this->raiseError('ordheadID not passed');
         } else {
             $dbeJOrdline = new DBEJOrdline($this);
-            $dbeJOrdline->setValue(DBEJOrdline::ordheadID, $ordheadID);
+            $dbeJOrdline->setValue(
+                DBEJOrdline::ordheadID,
+                $ordheadID
+            );
             $dbeJOrdline->getRowsByColumn(DBEJOrdline::ordheadID);
-            $ret = ($this->getData($dbeJOrdline, $dsOrdline));
+            $ret = ($this->getData(
+                $dbeJOrdline,
+                $dsOrdline
+            ));
             $dsOrdline->columnSort(DBEOrdline::sequenceNo);
         }
         return $ret;
@@ -71,15 +87,23 @@ class BUDespatch extends Business
     /**
      * Return a dataset of despatch qtys for this set of order lines
      */
-    function getInitialDespatchQtys(&$dsOrdline, &$dsDespatch)
+    function getInitialDespatchQtys(&$dsOrdline,
+                                    &$dsDespatch
+    )
     {
         $this->setMethodName('getInitialDespatchQtys');
         $this->initialiseDespatchDataset($dsDespatch);
         $dsOrdline->initialise();
         while ($dsOrdline->fetchNext()) {
             $dsDespatch->setUpdateModeInsert();
-            $dsDespatch->setValue('sequenceNo', $dsOrdline->getValue(DBEJOrdline::sequenceNo));
-            $dsDespatch->setValue('qtyToDespatch', 0);    // comment line
+            $dsDespatch->setValue(
+                'sequenceNo',
+                $dsOrdline->getValue(DBEJOrdline::sequenceNo)
+            );
+            $dsDespatch->setValue(
+                'qtyToDespatch',
+                0
+            );    // comment line
             $dsDespatch->post();
         }
         return TRUE;
@@ -88,8 +112,16 @@ class BUDespatch extends Business
     function initialiseDespatchDataset(&$dsDespatch)
     {
         $dsDespatch = new DataSet($this);
-        $dsDespatch->addColumn('sequenceNo', DA_INTEGER, DA_ALLOW_NULL);
-        $dsDespatch->addColumn('qtyToDespatch', DA_FLOAT, DA_ALLOW_NULL);
+        $dsDespatch->addColumn(
+            'sequenceNo',
+            DA_INTEGER,
+            DA_ALLOW_NULL
+        );
+        $dsDespatch->addColumn(
+            'qtyToDespatch',
+            DA_FLOAT,
+            DA_ALLOW_NULL
+        );
     }
 
     function getAllDeliveryMethods(&$dsResults)
@@ -97,25 +129,40 @@ class BUDespatch extends Business
         $this->setMethodName('getAllDeliveryMethods');
         $dbeDeliveryMethod = new DBEDeliveryMethod($this);
         $dbeDeliveryMethod->getRows();
-        return ($this->getData($dbeDeliveryMethod, $dsResults));
+        return ($this->getData(
+            $dbeDeliveryMethod,
+            $dsResults
+        ));
     }
 
-    function despatch($ordheadID, $deliveryMethodID, &$dsDespatch, $onlyCreateDespatchNote = true)
+    function despatch($ordheadID,
+                      $deliveryMethodID,
+                      &$dsDespatch,
+                      $onlyCreateDespatchNote = true
+    )
     {
         $this->setMethodName('despatch');
         $this->dbeOrdline = new DBEOrdline($this);
         $dsDespatch->initialise();
         $buSalesOrder = new BUSalesOrder($this);
-        $buSalesOrder->getOrdheadByID($ordheadID, $dsOrdhead);
+        $buSalesOrder->getOrdheadByID(
+            $ordheadID,
+            $dsOrdhead
+        );
         $partInvoice = ($dsOrdhead->getValue(DBEJOrdhead::partInvoice) == 'Y');
-        $this->getLinesByID($ordheadID, $dsOrdline);
+        $this->getLinesByID(
+            $ordheadID,
+            $dsOrdline
+        );
 
 // Check Whether The Order Will Be Complete Following This Despatch
         $fullyDespatched = TRUE;
         while ($dsDespatch->fetchNext()) {
             $dsOrdline->fetchNext();
             if ($dsOrdline->getValue(DBEOrdline::lineType) == 'I') {
-                $qtyOutstanding = $dsOrdline->getValue(DBEOrdline::qtyOrdered) - $dsOrdline->getValue(DBEOrdline::qtyDespatched);
+                $qtyOutstanding = $dsOrdline->getValue(DBEOrdline::qtyOrdered) - $dsOrdline->getValue(
+                        DBEOrdline::qtyDespatched
+                    );
                 if ($qtyOutstanding - $dsDespatch->getValue('qtyToDespatch') != 0) {
                     $fullyDespatched = FALSE;
                     break;
@@ -144,7 +191,11 @@ class BUDespatch extends Business
 
                 if (!$onlyCreateDespatchNote) {
 
-                    $this->updateOrdline($ordheadID, $dsOrdline, $dsDespatch);
+                    $this->updateOrdline(
+                        $ordheadID,
+                        $dsOrdline,
+                        $dsDespatch
+                    );
                 }
 
                 $ordlineUpdated = TRUE;
@@ -159,9 +210,15 @@ class BUDespatch extends Business
             $dbeOrdhead = new DBEOrdhead($this);
             $dbeOrdhead->getRow($ordheadID);
             if ($fullyDespatched) {
-                $dbeOrdhead->setValue(DBEOrdhead::type, 'C');
+                $dbeOrdhead->setValue(
+                    DBEOrdhead::type,
+                    'C'
+                );
             } else {
-                $dbeOrdhead->setValue(DBEOrdhead::type, 'P');
+                $dbeOrdhead->setValue(
+                    DBEOrdhead::type,
+                    'P'
+                );
             }
             $dbeOrdhead->updateRow();
             unset($dbeOrdhead);
@@ -176,12 +233,19 @@ class BUDespatch extends Business
                 // Last despatch for this non part-invoice order so generate invoice for whole
                 if (!$partInvoice AND $fullyDespatched) {
                     $buInvoice = new BUInvoice($this);
-                    $invheadID = $buInvoice->createInvoiceFromOrder($dsOrdhead, $dsOrdline);
+                    $invheadID = $buInvoice->createInvoiceFromOrder(
+                        $dsOrdhead,
+                        $dsOrdline
+                    );
                     unset($buInvoice);
                 }
                 if ($partInvoice) {
                     $buInvoice = new BUInvoice($this);
-                    $invheadID = $buInvoice->createInvoiceFromDespatch($dsOrdhead, $dsOrdline, $dsDespatch);
+                    $invheadID = $buInvoice->createInvoiceFromDespatch(
+                        $dsOrdhead,
+                        $dsOrdline,
+                        $dsDespatch
+                    );
                     unset($buInvoice);
                 }
             } // end
@@ -208,7 +272,11 @@ class BUDespatch extends Business
         }
 
         $dbeDeliveryMethod = new DBEDeliveryMethod($this);
-        $this->getDatasetByPK($deliveryMethodID, $dbeDeliveryMethod, $dsDeliveryMethod);
+        $this->getDatasetByPK(
+            $deliveryMethodID,
+            $dbeDeliveryMethod,
+            $dsDeliveryMethod
+        );
 
         $deliveryNoteFile = FALSE;
 
@@ -218,9 +286,19 @@ class BUDespatch extends Business
 
                 $buContact = new BUContact($this);
 
-                $buContact->getContactByID($dsOrdhead->getValue(DBEOrdhead::delContactID), $dsContact);
+                $buContact->getContactByID(
+                    $dsOrdhead->getValue(DBEOrdhead::delContactID),
+                    $dsContact
+                );
 
-                $deliveryNoteFile = $this->createDeliveryNote($dsOrdhead, $dsOrdline, $dsDespatch, $dsContact, $dsDeliveryMethod, $fullyDespatched);
+                $deliveryNoteFile = $this->createDeliveryNote(
+                    $dsOrdhead,
+                    $dsOrdline,
+                    $dsDespatch,
+                    $dsContact,
+                    $dsDeliveryMethod,
+                    $fullyDespatched
+                );
             }
         }
         unset($dbeDeliveryMethod);
@@ -238,13 +316,25 @@ class BUDespatch extends Business
     {
         // create record on delivery note table
         $dbeDeliveryNote = new DBEDeliveryNote($this);
-        $dbeDeliveryNote->setValue(DBEDeliveryNote::ordheadID, $dsOrdhead->getValue(DBEOrdhead::ordheadID));
+        $dbeDeliveryNote->setValue(
+            DBEDeliveryNote::ordheadID,
+            $dsOrdhead->getValue(DBEOrdhead::ordheadID)
+        );
         $noteNo = $dbeDeliveryNote->getNextNoteNo();
 
         $dbeDeliveryNote->setPKValue('0');
-        $dbeDeliveryNote->setValue(DBEDeliveryNote::ordheadID, $dsOrdhead->getValue(DBEOrdhead::ordheadID));
-        $dbeDeliveryNote->setValue(DBEDeliveryNote::noteNo, $noteNo);
-        $dbeDeliveryNote->setValue(DBEDeliveryNote::dateTime, date('Y-m-d H:i:s'));
+        $dbeDeliveryNote->setValue(
+            DBEDeliveryNote::ordheadID,
+            $dsOrdhead->getValue(DBEOrdhead::ordheadID)
+        );
+        $dbeDeliveryNote->setValue(
+            DBEDeliveryNote::noteNo,
+            $noteNo
+        );
+        $dbeDeliveryNote->setValue(
+            DBEDeliveryNote::dateTime,
+            date('Y-m-d H:i:s')
+        );
         $dbeDeliveryNote->insertRow();
         $buPDFDeliveryNote = new BUPDFDeliveryNote(
             $this,
@@ -259,22 +349,39 @@ class BUDespatch extends Business
         return ($buPDFDeliveryNote->generateFile()); // the file path is returned
     }
 
-    function updateOrdline($ordheadID, &$dsOrdline, &$dsDespatch)
+    function updateOrdline($ordheadID,
+                           &$dsOrdline,
+                           &$dsDespatch
+    )
     {
         $dbeOrdline = &$this->dbeOrdline;
-        $dbeOrdline->setValue(DBEOrdline::ordheadID, $ordheadID);
-        $dbeOrdline->setValue(DBEOrdline::sequenceNo, $dsOrdline->getValue(DBEOrdline::sequenceNo));
+        $dbeOrdline->setValue(
+            DBEOrdline::ordheadID,
+            $ordheadID
+        );
+        $dbeOrdline->setValue(
+            DBEOrdline::sequenceNo,
+            $dsOrdline->getValue(DBEOrdline::sequenceNo)
+        );
         $dbeOrdline->getRow();
         $dbeOrdline->setValue(
             DBEOrdline::qtyDespatched,
             $dsOrdline->getValue(DBEOrdline::qtyDespatched) + $dsDespatch->getValue('qtyToDespatch')
         );
-        $dbeOrdline->setValue(DBEOrdline::qtyLastDespatched, $dsDespatch->getValue('qtyToDespatch'));
-        $dbeOrdline->setValue(DBEOrdline::description, $dsOrdline->getValue(DBEOrdline::description));
+        $dbeOrdline->setValue(
+            DBEOrdline::qtyLastDespatched,
+            $dsDespatch->getValue('qtyToDespatch')
+        );
+        $dbeOrdline->setValue(
+            DBEOrdline::description,
+            $dsOrdline->getValue(DBEOrdline::description)
+        );
         $dbeOrdline->updateRow();
     }
 
-    function getDeliveryNotesByOrdheadID($ordheadID, &$dsDeliveryNote)
+    function getDeliveryNotesByOrdheadID($ordheadID,
+                                         &$dsDeliveryNote
+    )
     {
         $this->setMethodName('getDeliveryNotesByOrdheadID');
         $ret = FALSE;
@@ -282,21 +389,36 @@ class BUDespatch extends Business
             $this->raiseError('ordheadID not passed');
         }
         $dbeDeliveryNote = new DBEDeliveryNote($this);
-        $dbeDeliveryNote->setValue(DBEDeliveryNote::ordheadID, $ordheadID);
+        $dbeDeliveryNote->setValue(
+            DBEDeliveryNote::ordheadID,
+            $ordheadID
+        );
         $dbeDeliveryNote->getRowsByColumn(DBEDeliveryNote::ordheadID);
-        $ret = ($this->getData($dbeDeliveryNote, $dsDeliveryNote));
-        $dsDeliveryNote->columnSort('dateTime', SORT_DESC);
+        $ret = ($this->getData(
+            $dbeDeliveryNote,
+            $dsDeliveryNote
+        ));
+        $dsDeliveryNote->columnSort(
+            'dateTime',
+            SORT_DESC
+        );
         return $ret;
     }
 
-    function getDeliveryNoteByID($ID, &$dsDeliveryNote)
+    function getDeliveryNoteByID($ID,
+                                 &$dsDeliveryNote
+    )
     {
         $this->setMethodName('getDeliveryNoteByID');
         if ($ID == '') {
             $this->raiseError('deliveryNoteID not passed');
         }
         $dbeDeliveryNote = new DBEDeliveryNote($this);
-        return ($this->getDatasetByPK($ID, $dbeDeliveryNote, $dsDeliveryNote));
+        return ($this->getDatasetByPK(
+            $ID,
+            $dbeDeliveryNote,
+            $dsDeliveryNote
+        ));
     }
 
     function countNonReceievedPOsByOrdheadID($ID)
