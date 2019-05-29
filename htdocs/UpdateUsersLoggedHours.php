7aee461b657bb6b0f1da25aa5c74203c9d2f97e9
fizdalf <fizdalf@gmail.com>
2019-05-29 08:41:05 +0100

    fix small issues

--- htdocs/UpdateUsersLoggedHours.php
+++ htdocs/UpdateUsersLoggedHours.php
@@ -3,8 +3,14 @@ require_once("config.inc.php");
 
 require_once($cfg["path_bu"] . "/BUActivity.inc.php");
 
-$date = @$_REQUEST['date'];
+$date = new DateTime('today');
+if (isset($_REQUEST['date'])) {
+    $possibleDate = DateTime::createFromFormat('Y-m-d', $_REQUEST['date']);
+    if ($possibleDate) {
+        $date = $possibleDate;
+    }
+}
 $thing = null;
 $buActivity = new BUActivity($thing);
-$buActivity->updateAllHistoricUserLoggedHours(new DateTime('today'));
+$buActivity->updateAllHistoricUserLoggedHours($date);
 ?>
