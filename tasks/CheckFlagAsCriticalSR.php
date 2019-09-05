01421429ddab97fe1afbfea0fbc3413407e8eaed
fizdalf <fizdalf@gmail.com>
2019-09-05 12:43:43 +0100

    Fix reference to config file

--- tasks/CheckFlagAsCriticalSR.php
+++ tasks/CheckFlagAsCriticalSR.php
@@ -10,7 +10,7 @@
 
 use CNCLTD\LoggerCLI;
 
-require_once("htdocs/config.inc.php");
+require_once(__DIR__."/../htdocs/config.inc.php");
 require_once($cfg["path_dbe"] . "/DBEProblem.inc.php");
 require_once($cfg['path_bu'] . '/BUHeader.inc.php');
 global $db;
