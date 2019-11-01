458d43b802b2db4fa97d71fef99cee539dd150c4
fizdalf <fizdalf@gmail.com>
2019-11-01 09:36:13 +0000

    Create Controller skeleton

---
+++ cnccode/controller_classes/CTCreateSalesRequest.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fizdalf
+ * Date: 22/08/2018
+ * Time: 10:39
+ */
+global $cfg;
+require_once($cfg['path_ct'] . '/CTCNC.inc.php');
+
+class CTCreateSalesRequest extends CTCNC
+{
+    function __construct($requestMethod,
+                         $postVars,
+                         $getVars,
+                         $cookieVars,
+                         $cfg
+    )
+    {
+        parent::__construct(
+            $requestMethod,
+            $postVars,
+            $getVars,
+            $cookieVars,
+            $cfg
+        );
+        $roles = [
+            "sales",
+        ];
+        if (!self::hasPermissions($roles)) {
+            Header("Location: /NotAllowed.php");
+            exit;
+        }
+    }
+
+    /**
+     * Route to function based upon action passed
+     */
+    function defaultAction()
+    {
+
+        switch ($this->getAction()) {
+            default:
+                $this->showPage();
+                break;
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    function showPage()
+    {
+        $this->setTemplateFiles(
+            'ChangeRequestDashboard',
+            'ChangeRequestDashboard'
+        );
+
+        $this->setPageTitle('Change Request Dashboard');
+
+        $dbejCallActivity = new DBEJCallActivity($this);
+        $dbejCallActivity->getPendingChangeRequestRows();
+
+        $this->template->set_block(
+            'ChangeRequestDashboard',
+            'ChangeRequestsBlock',
+            'changeRequests'
+        );
+
+        $buActivity = new BUActivity($this);
+
+        while ($dbejCallActivity->fetchNext()) {
+
+            $lastActivity = $buActivity->getLastActivityInProblem(
+                $dbejCallActivity->getValue(DBEJCallActivity::problemID)
+            );
+            $srLink = Controller::buildLink(
+                'Activity.php',
+                [
+                    "callActivityID" => $lastActivity->getValue(DBEJCallActivity::callActivityID),
+                    "action"         => "displayActivity"
+                ]
+            );
+
+            $srLink = "<a href='$srLink'>SR</a>";
+
+//            http://cncdev7:85/Activity.php?action=changeRequestReview&callActivityID=1813051&fromEmail=true
+
+            $processCRLink = Controller::buildLink(
+                'Activity.php',
+                [
+                    "callActivityID" => $dbejCallActivity->getValue(DBEJCallActivity::callActivityID),
+                    "action"         => "changeRequestReview"
+                ]
+            );
+
+            $processCRLink = "<a href='$processCRLink'>Process Change Request</a>";
+
+            $this->template->set_var(
+                [
+                    'customerName'      => $dbejCallActivity->getValue(DBEJCallActivity::customerName),
+                    'srLink'            => $srLink,
+                    'changeRequested'   => $dbejCallActivity->getValue(DBEJCallActivity::reason),
+                    'requestedBy'       => $dbejCallActivity->getValue(DBEJCallActivity::userAccount),
+                    'requestedDateTime' => $dbejCallActivity->getValue(
+                            DBEJCallActivity::date
+                        ) . ' ' . $dbejCallActivity->getValue(DBEJCallActivity::startTime),
+                    'processCRLink'     => $processCRLink,
+                ]
+            );
+
+            $this->template->parse(
+                'changeRequests',
+                'ChangeRequestsBlock',
+                true
+            );
+        }
+
+
+        $this->template->parse(
+            'CONTENTS',
+            'ChangeRequestDashboard',
+            true
+        );
+        $this->parsePage();
+
+
+    }
+}
