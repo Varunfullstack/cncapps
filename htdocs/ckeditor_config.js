cf679b740a0a52707814e06175826f1c47bc162d
fizdalf <fizdalf@gmail.com>
2020-03-05 09:42:41 +0000

    Add ckeditor plugin to track character count
    Prevent pasting

--- htdocs/ckeditor_config.js
+++ htdocs/ckeditor_config.js
@@ -19,8 +19,13 @@ CKEDITOR.editorConfig = function (config) {
             ['Anchor', 'Link'],
             ['Undo', 'Redo']
         ];
-    config.extraPlugins = 'font';
-    config.fontSize_sizes = '8/8pt;9/9pt;10/10pt;11/11pt;12/12pt;14/14pt;16/16pt;18/18pt;20/20pt;22/22pt;24/24pt;26/26pt;28/28pt;36/36pt;48/48pt;72/72pt'
+    config.extraPlugins = 'font,wordcount';
+    config.fontSize_sizes = '8/8pt;9/9pt;10/10pt;11/11pt;12/12pt;14/14pt;16/16pt;18/18pt;20/20pt;22/22pt;24/24pt;26/26pt;28/28pt;36/36pt;48/48pt;72/72pt';
+    config.wordcount = {
+        showParagraphs: false,
+        showCharCount: true,
+        minCharCount: 200
+    };
     CKEDITOR.config.width = '870';
     CKEDITOR.config.height = '220';
     CKEDITOR.config.resize_minWidth = '760';
