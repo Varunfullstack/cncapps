a19365cc2685b72579614220fdba6015fb0b240c
fizdalf <fizdalf@gmail.com>
2020-04-02 13:43:27 +0100

    make the quantity fields for starters and leavers rows links to search SRs using the current criteria

--- cnccode/controller_classes/CTStartersAndLeaversReport.php
+++ cnccode/controller_classes/CTStartersAndLeaversReport.php
@@ -6,6 +6,7 @@
  * @access public
  * @authors Karim Ahmed - Sweet Code Limited
  */
+global $cfg;
 require_once($cfg ['path_ct'] . '/CTCNC.inc.php');
 require_once($cfg ['path_bu'] . '/BUStartersAndLeaversReport.php');
 require_once($cfg ['path_bu'] . '/BUCustomer.inc.php');
