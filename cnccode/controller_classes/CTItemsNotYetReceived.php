8b251a1fa164eda4429effaef3e73103ea0f6b65
fizdalf <fizdalf@gmail.com>
2020-05-26 10:50:14 +0100

    fix issues with date sorting

--- cnccode/controller_classes/CTItemsNotYetReceived.php
+++ cnccode/controller_classes/CTItemsNotYetReceived.php
@@ -84,67 +84,6 @@ class CTItemsNotYetReceived extends CTCNC
             'ItemsNotYetReceived',
             'ItemsNotYetReceived'
         );
-//        $daysAgo = 7;
-//        if ($this->getParam('daysAgo')) {
-//            $daysAgo = $this->getParam('daysAgo');
-//        }
-//        $itemsNotYetReceived = $this->buItemsNotYetReceived->getItemsNotYetReceived($daysAgo);
-
-//
-//        $this->template->set_block(
-//            'ItemsNotYetReceived',
-//            'notYetReceivedItemBlock',
-//            'notYetReceivedItems'
-//        );
-
-//        foreach ($itemsNotYetReceived as $item) {
-//
-//            $this->template->set_var(
-//                [
-//                    "style"                 => $style,
-//                    "purchaseOrderLink"     => $purchaseOrderLink,
-//                    "purchaseOrderId"       => $item->getPurchaseOrderId(),
-//                    "customerName"          => $item->getCustomerName(),
-//                    "itemDescription"       => $item->getItemDescription(),
-//                    "supplierName"          => $item->getSupplierName(),
-//                    "orderedQty"            => $item->getOrderedQuantity(),
-//                    "direct"                => $item->getDirect(),
-//                    "purchaseOrderDate"     => $this->getDateOrNA($item->getPurchaseOrderDate()),
-//                    "purchaseOrderDateSort" => $item->getPurchaseOrderDate() ? $item->getPurchaseOrderDate()->format(
-//                        DATE_MYSQL_DATE
-//                    ) : null,
-//                    "expectedOn"            => $purchaseOrderLineLink,
-//                    "expectedOnSort"        => $item->getExpectedOn() ? $item->getExpectedOn()->format(
-//                        DATE_MYSQL_DATE
-//                    ) : null,
-//                    "futureDate"            => $this->getDateOrNA($item->getFutureDate()),
-//                    "futureDateSort"        => $item->getFutureDate() ? $item->getFutureDate()->format(
-//                        DATE_MYSQL_DATE
-//                    ) : null,
-//                    "requiredByDate"        => $item->getPurchaseOrderRequiredBy() ? $item->getPurchaseOrderRequiredBy(
-//                    )->format('d/m/Y') : 'TBC',
-//                    "requiredByDateSort"    => $item->getPurchaseOrderRequiredBy() ? $item->getPurchaseOrderRequiredBy(
-//                    )->format(DATE_MYSQL_DATE) : null,
-//                    "requiredByColor"       => $item->getRequiredByColorClass(
-//                    ) ? "class='{$item->getRequiredByColorClass()}'" : null,
-//                    "supplierRef"           => $item->getSupplierRef(),
-//                    "color"                 => $item->color(),
-//                    "expectedColor"         => $item->getExpectedColorClass(
-//                    ) ? "class='{$item->getExpectedColorClass()}'" : null,
-//                    "projectLink"           => $projectLink,
-//                    "salesOrderLink"        => $salesOrderLink,
-//                    "SRLink"                => $serviceRequestLink
-//                ]
-//            );
-//
-//            $this->template->parse(
-//                'notYetReceivedItems',
-//                'notYetReceivedItemBlock',
-//                true
-//            );
-//        }
-
-
         $this->template->parse(
             'CONTENTS',
             'ItemsNotYetReceived',
