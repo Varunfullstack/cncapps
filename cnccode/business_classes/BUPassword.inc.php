7a054f37358dc8837064a99bb0e5f34bfcb4ba5b
Mustafa Taha <dev.mtaha@gmail.com>
2020-06-18 14:07:12 +0200

    allow Double Quote

--- cnccode/business_classes/BUPassword.inc.php
+++ cnccode/business_classes/BUPassword.inc.php
@@ -132,7 +132,6 @@ class BUPassword extends Business
         if (!$data) {
             return null;
         }
-        $data=trim($data," ");
         return Encryption::encrypt(
             PASSWORD_ENCRYPTION_PUBLIC_KEY,
             $data
