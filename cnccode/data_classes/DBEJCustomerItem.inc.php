c243d3eb056e28d2f12b2df103beeca061de0d2e
fizdalf <fizdalf@gmail.com>
2020-08-26 18:46:54 +0100

    Fix issue

--- cnccode/data_classes/DBEJCustomerItem.inc.php
+++ cnccode/data_classes/DBEJCustomerItem.inc.php
@@ -136,7 +136,7 @@ class DBEJCustomerItem extends DBECustomerItem
                         JOIN item AS citem ON cui_itemno = itm_itemno 
                         JOIN customer ON cui_custno = cus_custno 
                         JOIN address ON add_siteno = cui_siteno AND add_custno = cui_custno
-                        left join itemtype on ity_itemtypeno = citem.itm_itemno
+                        left join itemtype on ity_itemtypeno = citem.itm_itemtypeno
                         ";
 
 
@@ -224,9 +224,11 @@ class DBEJCustomerItem extends DBECustomerItem
                 JOIN item AS citem ON cui_itemno = itm_itemno 
                 JOIN customer ON cui_custno = cus_custno 
                 JOIN address ON add_siteno = cui_siteno AND add_custno = cui_custno
-                join itemtype on ity_itemtypeno = citem.itm_itemno
+                join itemtype on ity_itemtypeno = citem.itm_itemtypeno
             WHERE {$this->getDBColumnName(self::customerItemID)}={$ID}";
         $this->setQueryString($queryString);
+        var_dump($queryString);
+        exit;
         return (parent::getRow());
     }
 
@@ -249,7 +251,7 @@ class DBEJCustomerItem extends DBECustomerItem
                 JOIN item AS citem ON cui_itemno = itm_itemno 
                 JOIN customer ON cui_custno = cus_custno 
                 JOIN address ON add_siteno = cui_siteno AND add_custno = cui_custno
-                join itemtype on ity_itemtypeno = citem.itm_itemno
+                join itemtype on ity_itemtypeno = citem.itm_itemtypeno
                 WHERE {$this->getDBColumnName($ixColumn)}={$this->getFormattedValue($ixColumn)}";
 
         if ($sortColumn != '') {
@@ -277,7 +279,7 @@ class DBEJCustomerItem extends DBECustomerItem
           JOIN item AS citem ON cui_itemno = itm_itemno
           JOIN customer ON cui_custno = cus_custno
           JOIN address ON add_siteno = cui_siteno AND add_custno = cui_custno
-          join itemtype on ity_itemtypeno = citem.itm_itemno
+          join itemtype on ity_itemtypeno = citem.itm_itemtypeno
        WHERE
           cic_contractcuino = $customerItemId
 
@@ -309,7 +311,7 @@ class DBEJCustomerItem extends DBECustomerItem
           JOIN custitem_contract cic ON cic.cic_cuino = custitem.cui_cuino
           JOIN custitem con ON con.cui_cuino = cic.cic_contractcuino
           JOIN item con_item ON con.cui_itemno = con_item.itm_itemno
-          join itemtype on ity_itemtypeno = citem.itm_itemno
+          join itemtype on ity_itemtypeno = citem.itm_itemtypeno
       
  		   WHERE " . $this->getDBColumnName(self::customerID) . "=" . $this->getValue(self::customerID) .
             " AND citem.itm_itemtypeno = " . CONFIG_SERVER_ITEMTYPEID .
