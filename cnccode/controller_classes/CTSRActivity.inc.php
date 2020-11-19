059eb3e358b2cb04298633abbd726df8c0997d39
fizdalf <fizdalf@gmail.com>
2020-11-19 08:45:44 +0000

    Set awaiting customer flag in the problem correctly

--- cnccode/controller_classes/CTSRActivity.inc.php
+++ cnccode/controller_classes/CTSRActivity.inc.php
@@ -650,14 +650,15 @@ class CTSRActivity extends CTCNC
                 http_response_code(400);
                 return ["error" => 'Please provide an escalate reason'];
             }
-            if ($updateAwaitingCustomer) {
-                $dbeProblem->setValue(
-                    DBEProblem::awaitingCustomerResponseFlag,
-                    $dsCallActivity->getValue(DBECallActivity::awaitingCustomerResponseFlag)
-                );
-                $dbeProblem->updateRow();
-            }
             $dsCallActivity->post();
+        }
+
+        if ($updateAwaitingCustomer) {
+            $dbeProblem->setValue(
+                DBEProblem::awaitingCustomerResponseFlag,
+                $dsCallActivity->getValue(DBECallActivity::awaitingCustomerResponseFlag)
+            );
+            $dbeProblem->updateRow();
         }
 
         $enteredEndTime = $buActivity->updateCallActivity(
