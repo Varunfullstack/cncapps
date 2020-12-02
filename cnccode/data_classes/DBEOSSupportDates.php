21402957fc9b95fdac7529e33407bb5d1487e375
fizdalf <fizdalf@gmail.com>
2020-12-02 15:49:55 +0000

    Start creating field

--- cnccode/data_classes/DBEOSSupportDates.php
+++ cnccode/data_classes/DBEOSSupportDates.php
@@ -4,12 +4,13 @@ require_once($cfg["path_gc"] . "/DBEntity.inc.php");
 
 class DBEOSSupportDates extends DBEntity
 {
-    const id = "id";
-    const name = "name";
-    const version = "version";
+    const id               = "id";
+    const name             = "name";
+    const version          = "version";
     const availabilityDate = "availabilityDate";
-    const endOfLifeDate = "endOfLifeDate";
-    const isServer = 'isServer';
+    const endOfLifeDate    = "endOfLifeDate";
+    const isServer         = 'isServer';
+    const friendlyName     = "friendlyName";
 
     public function __construct(&$owner)
     {
@@ -21,6 +22,7 @@ class DBEOSSupportDates extends DBEntity
         $this->addColumn(self::availabilityDate, DA_DATE, DA_NOT_NULL);
         $this->addColumn(self::endOfLifeDate, DA_DATE, DA_ALLOW_NULL);
         $this->addColumn(self::isServer, DA_BOOLEAN, DA_NOT_NULL, 0);
+        $this->addColumn(self::friendlyName, DA_STRING, DA_ALLOW_NULL);
         $this->setAddColumnsOff();
         $this->setPK(self::id);
     }
