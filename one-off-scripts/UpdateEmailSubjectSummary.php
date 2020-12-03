acf6ade28abf0e65de80e98b979f5c037094ce51
fizdalf <fizdalf@gmail.com>
2020-12-03 08:24:25 +0000

    Fix script as it was using the wrong array

--- one-off-scripts/UpdateEmailSubjectSummary.php
+++ one-off-scripts/UpdateEmailSubjectSummary.php
@@ -4,7 +4,7 @@ global $db;
 $fetchServiceRequestsToChange = '
 select problem.pro_problemno as serviceRequestId, callactivity.reason as initialReason  from
                 problem
-                    LEFT JOIN callactivity
+                    JOIN callactivity
                     ON callactivity.`caa_problemno` = problem.`pro_problemno`
                         AND callactivity.`caa_callacttypeno` = 51
             WHERE problem.`pro_status` IN ("I", "P", "F")
@@ -17,11 +17,11 @@ foreach ($serviceRequestsToUpdate as $serviceRequestToUpdate) {
         [
             [
                 "type"  => "s",
-                "value" => substr(strip_tags($serviceRequestsToUpdate['initialReason']), 0, 50)
+                "value" => substr(strip_tags($serviceRequestToUpdate['initialReason']), 0, 50)
             ],
             [
                 "type"  => "i",
-                "value" => $serviceRequestsToUpdate['serviceRequestId']
+                "value" => $serviceRequestToUpdate['serviceRequestId']
             ]
         ]
     );
