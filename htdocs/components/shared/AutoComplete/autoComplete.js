b0446c7d88b5dea90f2cb2a9ea5c019323f898a2
fizdalf <fizdalf@gmail.com>
2020-12-11 08:59:03 +0000

    Start working on custom asset box

--- htdocs/components/shared/AutoComplete/autoComplete.js
+++ htdocs/components/shared/AutoComplete/autoComplete.js
@@ -2,215 +2,216 @@
 import React from 'react';
 
 class AutoComplete extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      // The active selection's index
-      activeSuggestion: 0,
-      // The suggestions that match the user's input
-      filteredSuggestions: [],
-      // Whether or not the suggestion list is shown
-      showSuggestions: false,
-      // What the user has entered
-      userInput: "",
-      filtered:false
-    };
-
-  }
-  // Event fired when the input value is changed
-  onChange = (e) => {
-
-    let { items, displayLength ,displayColumn} = this.props;
-    if (!displayLength) displayLength = 10;
-    const userInput = e.currentTarget.value;
-
-    // Filter our suggestions that don't contain the user's input
-    let filteredSuggestions = items
-      .filter(
-        (suggestion) =>
-          (displayColumn?suggestion[displayColumn]:suggestion).toLowerCase().indexOf(userInput.toLowerCase()) > -1
-      );
-     if (filteredSuggestions.length > displayLength) {
-      filteredSuggestions = [
-        {
-          name: "Keep typing to filter, there are more results not shown here",
-          id: null,
-        },
-        ...filteredSuggestions,
-      ];
-    }
-    filteredSuggestions=filteredSuggestions.slice(0, displayLength);
-    // Update the user input and filtered suggestions, reset the active
-    // suggestion and make sure the suggestions are shown
-    this.setState({
-      activeSuggestion: e.currentTarget.value.length>0?0:-1,
-      filteredSuggestions,
-      showSuggestions: true,
-      userInput: e.currentTarget.value,
-      filtered:true
-    });
-  };
-  // Event fired when the user clicks on a suggestion
-  onClick = (item) => {
-    if(item.id!=null)
-    {
-    const {displayColumn,onSelect}=this.props;
-    // Update the user input and reset the rest of the state
-
-    this.setState({
-      activeSuggestion: 0,
-       showSuggestions: false,
-      userInput:displayColumn?item[displayColumn]:item,
-    });
-    if(onSelect)
-      onSelect(item);
-  }
-  };
-  // Event fired when the user presses a key down
-  onKeyDown = (e) => {
-    const { activeSuggestion, filteredSuggestions,userInput } = this.state;
-    const {displayColumn,onSelect}   =this.props;
-
-    if(userInput.length==0)
-    {
-      this.setState({
-        activeSuggestion: -1,
-        showSuggestions: false,
-        userInput: '',
-        filteredSuggestions:[]
-      });
-      onSelect(null);
-      return;
-    }
-    if(e.keyCode==27) //esc
-    {
-        this.setState({
+        this.state = {
+            // The active selection's index
             activeSuggestion: 0,
+            // The suggestions that match the user's input
+            filteredSuggestions: [],
+            // Whether or not the suggestion list is shown
             showSuggestions: false,
-            userInput: '',
-            filteredSuggestions:[]
-          });
-          return;
-    }
-    // User pressed the enter key, update the input and close the
-    // suggestions
-    if (e.keyCode == 13) {
-      this.setState({
-        activeSuggestion: 0,
-        showSuggestions: false,
-        userInput: displayColumn?filteredSuggestions[activeSuggestion][displayColumn]:filteredSuggestions[activeSuggestion],
-      });
-      if(onSelect)
-      onSelect(filteredSuggestions[activeSuggestion]);
-    }
-    // User pressed the up arrow, decrement the index
-    else if (e.keyCode == 38) {
-      if (activeSuggestion == 0) {
-        return;
-      }
+            // What the user has entered
+            userInput: "",
+            filtered: false
+        };
 
-      this.setState({ activeSuggestion: activeSuggestion - 1 });
     }
-    // User pressed the down arrow, increment the index
-    else if (e.keyCode == 40) {
-      if (activeSuggestion - 1 == filteredSuggestions.length) {
-        return;
-      }
 
-      this.setState({ activeSuggestion: activeSuggestion + 1 });
-    }
-  };
-  handleOnClick = e=>{
-     let {items,displayLength}=this.props;
-     const {userInput}=this.state;
-     displayLength=displayLength??10;
-     let {filteredSuggestions}=this.state;
-     if(filteredSuggestions.length==0&&userInput=='') // display first n of items
-     {
-        filteredSuggestions=items.slice(0,displayLength);
-        if (items.length > displayLength) {
-          filteredSuggestions = [
-            {
-              name: "Keep typing to filter, there are more results not shown here",
-              id: null,
-            },
-            ...filteredSuggestions,
-          ];
+    // Event fired when the input value is changed
+    onChange = (e) => {
+
+        let {items, displayLength, displayColumn} = this.props;
+        if (!displayLength) displayLength = 10;
+        const userInput = e.currentTarget.value;
+
+        // Filter our suggestions that don't contain the user's input
+        let filteredSuggestions = items
+            .filter(
+                (suggestion) =>
+                    (displayColumn ? suggestion[displayColumn] : suggestion).toLowerCase().indexOf(userInput.toLowerCase()) > -1
+            );
+        if (filteredSuggestions.length > displayLength) {
+            filteredSuggestions = [
+                {
+                    name: "Keep typing to filter, there are more results not shown here",
+                    id: null,
+                },
+                ...filteredSuggestions,
+            ];
         }
-        this.setState({filteredSuggestions,
-            showSuggestions:true
-        })
-     }
-     else
-     {
+        filteredSuggestions = filteredSuggestions.slice(0, displayLength);
+        // Update the user input and filtered suggestions, reset the active
+        // suggestion and make sure the suggestions are shown
         this.setState({
-            showSuggestions:true
-        })
-     }
-  }
-  handleOnBlur=e=>{
-      setTimeout(()=>{
-        this.setState({showSuggestions:false})
-      },200);
-  }
-  componentDidUpdate(prevProps) {
+            activeSuggestion: e.currentTarget.value.length > 0 ? 0 : -1,
+            filteredSuggestions,
+            showSuggestions: true,
+            userInput: e.currentTarget.value,
+            filtered: true
+        });
+    };
+    // Event fired when the user clicks on a suggestion
+    onClick = (item) => {
+        if (item.id != null) {
+            const {displayColumn, onSelect} = this.props;
+            // Update the user input and reset the rest of the state
 
-  }
-  render() {
-    const {displayColumn,pk,errorMessage,required,width}=this.props;
-    const {
-      onChange,
-      onClick,
-      onKeyDown,
-      state: {
-        activeSuggestion,
-        filteredSuggestions,
-        showSuggestions,
-        userInput,
-        filtered
-      },
-      handleOnClick,
-      handleOnBlur
-    } = this;
-    let suggestionsListComponent;
+            this.setState({
+                activeSuggestion: 0,
+                showSuggestions: false,
+                userInput: displayColumn ? item[displayColumn] : item,
+            });
+            if (onSelect)
+                onSelect(item);
+        }
+    };
+    // Event fired when the user presses a key down
+    onKeyDown = (e) => {
+        const {activeSuggestion, filteredSuggestions, userInput} = this.state;
+        const {displayColumn, onSelect} = this.props;
 
-    if (showSuggestions) {
-      if (filteredSuggestions.length) {
-        suggestionsListComponent = React.createElement("ul", {
-          className: "suggestions"
-        }, filteredSuggestions.map((suggestion, index) => {
-          let className; // Flag the active suggestion with a class
-        
-          if (index == activeSuggestion) {
-            className = "suggestion-active";
-          }
-         
-          return React.createElement("li", {
-            className: className,
-            key: pk ? suggestion[pk] : suggestion,
-            onClick: () => onClick(suggestion)
-          }, displayColumn ? suggestion[displayColumn] : suggestion);
-        }));
-      } else {
-        if(userInput!=="")
-        suggestionsListComponent =React.createElement("div", {
-          className: "no-suggestions"
-        }, React.createElement("em", null, errorMessage ? errorMessage : "No items "));
-      }
-    }    
-    let defaultValue=this.props.value?this.props.value:"";
-    return React.createElement("div", null, React.createElement("input", {
-      className:"form-control "+ (required ? "required" : ""),
-      type: "text",
-      onChange: onChange,
-      onKeyDown: onKeyDown,
-      value: userInput||(!filtered&&defaultValue)||"",
-      onClick: handleOnClick,
-      onBlur: handleOnBlur,
-      style:{width:width||'100%'},
-      autoComplete:"off",
-    }), suggestionsListComponent);
-  }
+        if (userInput.length == 0) {
+            this.setState({
+                activeSuggestion: -1,
+                showSuggestions: false,
+                userInput: '',
+                filteredSuggestions: []
+            });
+            onSelect(null);
+            return;
+        }
+        if (e.keyCode == 27) //esc
+        {
+            this.setState({
+                activeSuggestion: 0,
+                showSuggestions: false,
+                userInput: '',
+                filteredSuggestions: []
+            });
+            return;
+        }
+        // User pressed the enter key, update the input and close the
+        // suggestions
+        if (e.keyCode == 13) {
+            this.setState({
+                activeSuggestion: 0,
+                showSuggestions: false,
+                userInput: displayColumn ? filteredSuggestions[activeSuggestion][displayColumn] : filteredSuggestions[activeSuggestion],
+            });
+            if (onSelect)
+                onSelect(filteredSuggestions[activeSuggestion]);
+        }
+        // User pressed the up arrow, decrement the index
+        else if (e.keyCode == 38) {
+            if (activeSuggestion == 0) {
+                return;
+            }
+
+            this.setState({activeSuggestion: activeSuggestion - 1});
+        }
+        // User pressed the down arrow, increment the index
+        else if (e.keyCode == 40) {
+            if (activeSuggestion - 1 == filteredSuggestions.length) {
+                return;
+            }
+
+            this.setState({activeSuggestion: activeSuggestion + 1});
+        }
+    };
+    handleOnClick = e => {
+        let {items, displayLength} = this.props;
+        const {userInput} = this.state;
+        displayLength = displayLength ?? 10;
+        let {filteredSuggestions} = this.state;
+        if (filteredSuggestions.length == 0 && userInput == '') // display first n of items
+        {
+            filteredSuggestions = items.slice(0, displayLength);
+            if (items.length > displayLength) {
+                filteredSuggestions = [
+                    {
+                        name: "Keep typing to filter, there are more results not shown here",
+                        id: null,
+                    },
+                    ...filteredSuggestions,
+                ];
+            }
+            this.setState({
+                filteredSuggestions,
+                showSuggestions: true
+            })
+        } else {
+            this.setState({
+                showSuggestions: true
+            })
+        }
+    }
+    handleOnBlur = e => {
+        setTimeout(() => {
+            this.setState({showSuggestions: false})
+        }, 200);
+    }
+
+    componentDidUpdate(prevProps) {
+
+    }
+
+    render() {
+        const {displayColumn, pk, errorMessage, required, width} = this.props;
+        const {
+            onChange,
+            onClick,
+            onKeyDown,
+            state: {
+                activeSuggestion,
+                filteredSuggestions,
+                showSuggestions,
+                userInput,
+                filtered
+            },
+            handleOnClick,
+            handleOnBlur
+        } = this;
+        let suggestionsListComponent;
+
+        if (showSuggestions) {
+            if (filteredSuggestions.length) {
+                suggestionsListComponent = React.createElement("ul", {
+                    className: "suggestions"
+                }, filteredSuggestions.map((suggestion, index) => {
+                    let className; // Flag the active suggestion with a class
+
+                    if (index == activeSuggestion) {
+                        className = "suggestion-active";
+                    }
+
+                    return React.createElement("li", {
+                        className: className,
+                        key: pk ? suggestion[pk] : suggestion,
+                        onClick: () => onClick(suggestion)
+                    }, displayColumn ? suggestion[displayColumn] : suggestion);
+                }));
+            } else {
+                if (userInput !== "")
+                    suggestionsListComponent = React.createElement("div", {
+                        className: "no-suggestions"
+                    }, React.createElement("em", null, errorMessage ? errorMessage : "No items "));
+            }
+        }
+        let defaultValue = this.props.value ? this.props.value : "";
+        return React.createElement("div", null, React.createElement("input", {
+            className: "form-control " + (required ? "required" : ""),
+            type: "text",
+            onChange: onChange,
+            onKeyDown: onKeyDown,
+            value: userInput || (!filtered && defaultValue) || "",
+            onClick: handleOnClick,
+            onBlur: handleOnBlur,
+            style: {width: width || '100%'},
+            autoComplete: "off",
+        }), suggestionsListComponent);
+    }
 }
+
 export default AutoComplete;
