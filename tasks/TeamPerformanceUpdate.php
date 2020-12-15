2011e2dac0092a9d4ac4930d57764c73a753900c
fizdalf <fizdalf@gmail.com>
2020-12-15 17:28:01 +0000

    remove unused code

--- tasks/TeamPerformanceUpdate.php
+++ tasks/TeamPerformanceUpdate.php
@@ -53,24 +53,4 @@ $buTeamPerformance->update(
         strtotime("-1 months")
     )
 )
-//Uncomment this to update previous months
-/*
-$buTeamPerformance->update( 2015, 1 );
-$buTeamPerformance->update( 2015, 2 );
-$buTeamPerformance->update( 2015, 3 );
-$buTeamPerformance->update( 2015, 4 );
-$buTeamPerformance->update( 2015, 5 );
-$buTeamPerformance->update( 2015, 6 );
-$buTeamPerformance->update( 2015, 7 );
-$buTeamPerformance->update( 2015, 8 );
-$buTeamPerformance->update( 2015, 9 );
-$buTeamPerformance->update( 2015, 10 );
-$buTeamPerformance->update( 2015, 11 );
-$buTeamPerformance->update( 2015, 12 );
-$buTeamPerformance->update( 2016, 1 );
-$buTeamPerformance->update( 2016, 2 );
-$buTeamPerformance->update( 2016, 3 );
-$buTeamPerformance->update( 2016, 4 );
-$buTeamPerformance->update( 2016, 5 );
-*/
 ?>
