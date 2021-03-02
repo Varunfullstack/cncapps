5dba2030cce21e5db287e77a29c2a7e31ad2b9e1
cncltd <adrianc@cnc-ltd.co.uk>
2021-03-02 06:38:03 +0000

    Icon and tool tip changes

--- htdocs/components/CurrentActivityReportComponent/subComponents/CallBackModal.js
+++ htdocs/components/CurrentActivityReportComponent/subComponents/CallBackModal.js
@@ -46,7 +46,7 @@ class CallBackModal extends MainComponent {
         <div>
             
           <div className="form-group">
-            <label>Callback date / time</label>            
+            <label>Call back date / time</label>            
             <div className="flex-row">
               <input
                 type="date"
