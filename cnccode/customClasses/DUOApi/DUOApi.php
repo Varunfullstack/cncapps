7ab88598dbab026d615a1714be962d30be79c282
fizdalf <fizdalf@gmail.com>
2021-04-15 17:45:38 +0100

    Finish issue

--- cnccode/customClasses/DUOApi/DUOApi.php
+++ cnccode/customClasses/DUOApi/DUOApi.php
@@ -173,7 +173,8 @@ class DUOApi
         $params  = [
             "mintime" => $minTime->getTimestamp() * 1000,
             "maxtime" => $maxTime->getTimestamp() * 1000,
-            "limit"   => 1000
+            "limit"   => 1000,
+            "reasons" => "user_marked_fraud,deny_unenrolled_user,error,locked_out,user_disabled,user_cancelled,invalid_passcode,no_response,no_keys_pressed,call_timed_out,location_restricted,factor_restricted,platform_restricted,version_restricted,rooted_device,no_screen_lock,touch_id_disabled,no_disk_encryption,anonymous_ip,out_of_date,denied_by_policy,software_restricted,no_duo_certificate_present,could_not_determine_if_endpoint_was_trusted,invalid_management_certificate_collection_state,no_referring_hostname_provided,invalid_referring_hostname_provided,no_web_referer_match,endpoint_failed_google_verification,endpoint_is_not_trusted,invalid_device,anomalous_push,endpoint_is_not_in_management_system,no_activated_duo_mobile_account,allow_unenrolled_user,bypass_user,remembered_device,trusted_location,user_approved,valid_passcode,allowed_by_policy,user_not_in_permitted_group"
         ];
         if ($nextOffset) {
             $params['next_offset'] = implode(",", $nextOffset);
@@ -185,6 +186,9 @@ class DUOApi
         );
         if (!$response['success']) {
             throw new Exception('Failed to pull accounts list');
+        }
+        if ($response['http_status_code'] != 200) {
+            throw new Exception('Failed to pull data');
         }
         $jsonDecoder = new JsonDecoder();
         $jsonDecoder->register(new AccessDeviceLocationTransformer());
--- tasks/CheckDUO.php
+++ tasks/CheckDUO.php
@@ -67,7 +67,12 @@ foreach ($duoAPI->getAccountsList() as $account) {
         continue;
     }
     $dbeCustomer->fetchNext();
+    try {
+
     updateClientReport($clientDUO, $dbeCustomer);
+    } catch (\Exception $exception){
+        $logger->error('Could not update client DUO Excel Document');
+    }
     $dbeCustomerItem = new DBECustomerItem($thing);
     $dbeCustomerItem->getRowsByCustomerAndItemID(
         $dbeCustomer->getValue(DBECustomer::customerID),
