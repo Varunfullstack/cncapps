ef7c6bc934d38795a4779b1a7f697be1ce05ae13
fizdalf <fizdalf@gmail.com>
2021-04-28 12:13:50 +0100

    Update patch management contract

--- cnccode/data_classes/DBEOSSupportDates.php
+++ cnccode/data_classes/DBEOSSupportDates.php
@@ -4,14 +4,13 @@ require_once($cfg["path_gc"] . "/DBEntity.inc.php");
 
 class DBEOSSupportDates extends DBEntity
 {
-    const id                        = "id";
-    const name                      = "name";
-    const version                   = "version";
-    const availabilityDate          = "availabilityDate";
-    const endOfLifeDate             = "endOfLifeDate";
-    const isServer                  = 'isServer';
-    const friendlyName              = "friendlyName";
-    const thirdPartyPatchingCapable = "thirdPartyPatchingCapable";
+    const id               = "id";
+    const name             = "name";
+    const version          = "version";
+    const availabilityDate = "availabilityDate";
+    const endOfLifeDate    = "endOfLifeDate";
+    const isServer         = 'isServer';
+    const friendlyName     = "friendlyName";
 
     public function __construct(&$owner)
     {
@@ -22,16 +21,15 @@ class DBEOSSupportDates extends DBEntity
         $this->addColumn(self::version, DA_STRING, DA_NOT_NULL);
         $this->addColumn(self::availabilityDate, DA_DATE, DA_NOT_NULL);
         $this->addColumn(self::endOfLifeDate, DA_DATE, DA_ALLOW_NULL);
-        $this->addColumn(self::isServer, DA_BOOLEAN, DA_NOT_NULL, null, 0);
+        $this->addColumn(self::isServer, DA_BOOLEAN, DA_NOT_NULL, 0);
         $this->addColumn(self::friendlyName, DA_STRING, DA_ALLOW_NULL);
-        $this->addColumn(self::thirdPartyPatchingCapable, DA_BOOLEAN, DA_NOT_NULL, null, 0);
         $this->setAddColumnsOff();
         $this->setPK(self::id);
     }
 
     public function getEndOfLifeRows()
     {
-        $query             = "select {$this->getFullDBColumnNamesAsString()} from {$this->getTableName()} where {$this->getDBColumnName(self::endOfLifeDate)} is not null";
+        $query = "select {$this->getFullDBColumnNamesAsString()} from {$this->getTableName()} where {$this->getDBColumnName(self::endOfLifeDate)} is not null";
         $this->queryString = $query;
         $this->getRows();
     }
