c4f07816494543835db74b6e09528b7f60528818
fizdalf <fizdalf@gmail.com>
2021-04-30 14:11:03 +0100

    Get the right information to display the selected asset

--- htdocs/components/shared/AssetListSelectorComponent/AssetListSelectorComponent.js
+++ htdocs/components/shared/AssetListSelectorComponent/AssetListSelectorComponent.js
@@ -5,6 +5,8 @@ import React from 'react';
 
 import striptags from "striptags";
 import ToolTip from "../ToolTip";
+import CNCCKEditor from "../CNCCKEditor";
+import PropTypes from "prop-types";
 
 export const ASSET_SELECTED_TYPE = {
     NO_ASSET_REASON: "NO_ASSET_REASON",
@@ -260,5 +262,12 @@ export default class AssetListSelectorComponent extends React.PureComponent {
             />
         );
     }
-
 }
+
+AssetListSelectorComponent.propTypes = {
+    noAssetReason: PropTypes.string,
+    assetName: PropTypes.string,
+    assetTitle: PropTypes.string,
+    customerId: PropTypes.number,
+    onChange: PropTypes.func
+};
