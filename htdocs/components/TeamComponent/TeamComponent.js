d56cfc4a6eab17eb28853d503486cc7c7a0f022f
fizdalf <fizdalf@gmail.com>
2021-06-02 11:08:27 +0100

    finish issue

--- htdocs/components/TeamComponent/TeamComponent.js
+++ htdocs/components/TeamComponent/TeamComponent.js
@@ -1,6 +1,6 @@
 import MainComponent from "../shared/MainComponent.js";
 import React from "react";
-import ReactDOM from "react-dom"; 
+import ReactDOM from "react-dom";
 import Spinner from "../shared/Spinner/Spinner";
 import Table from "../shared/table/table.js";
 import ToolTip from "../shared/ToolTip.js";
@@ -12,55 +12,56 @@ import APIUser from "../services/APIUser.js";
 import Toggle from "../shared/Toggle.js";
 
 class TeamComponent extends MainComponent {
-   api=new APITeam();
-   apiUsers=new APIUser();
+    api = new APITeam();
+    apiUsers = new APIUser();
+
     constructor(props) {
         super(props);
         this.state = {
-            ...this.state,    
-            showSpinner:false ,
-            showModal:false,
-            teams:[]   ,
-            mode:"new"   ,
-            data:{
-                teamID:'',
-                name:'',     
-                teamRoleName:''            ,
-                level:'',
-                activeFlag:'Y',
-                leaderName:'',
-                canDelete:false,
-                teamRoleID:'',
-                leaderId:'',
-                
+            ...this.state,
+            showSpinner: false,
+            showModal: false,
+            teams: [],
+            mode: "new",
+            data: {
+                teamID: '',
+                name: '',
+                teamRoleName: '',
+                level: '',
+                activeFlag: 'Y',
+                leaderName: '',
+                canDelete: false,
+                teamRoleID: '',
+                leaderId: '',
+
             },
-            roles:[],
-            users:[]
+            roles: [],
+            users: []
         };
     }
 
-    componentDidMount() {  
+    componentDidMount() {
         this.getData();
-        this.api.getRoles().then(res=>this.setState({roles:res.data}));
-        this.apiUsers.getAllUsers().then(users=>this.setState({users}));
+        this.api.getRoles().then(res => this.setState({roles: res.data}));
+        this.apiUsers.getAllUsers().then(users => this.setState({users}));
     }
 
-    getData=()=>{
-        this.setState({showSpinner:true})
-        this.api.getAllTeams().then(res=>{
-            
-            this.setState({teams:res,showSpinner:false});
+    getData = () => {
+        this.setState({showSpinner: true})
+        this.api.getAllTeams().then(res => {
+
+            this.setState({teams: res, showSpinner: false});
         });
     }
 
-    getDataTable=()=>{
-        const columns=[
+    getDataTable = () => {
+        const columns = [
             {
-               path: "name",
-               label: "Name",
-               hdToolTip: "Name",
-               hdClassName: "text-center",
-               sortable: true,
+                path: "name",
+                label: "Name",
+                hdToolTip: "Name",
+                hdClassName: "text-center",
+                sortable: true,
             },
             {
                 path: "teamRoleName",
@@ -68,124 +69,134 @@ class TeamComponent extends MainComponent {
                 hdToolTip: "Role",
                 hdClassName: "text-center",
                 sortable: true,
-             },
-             {
+            },
+            {
                 path: "leaderName",
                 label: "Leader",
                 hdToolTip: "Leader",
                 hdClassName: "text-center",
                 sortable: true,
-             },
-             {
+            },
+            {
                 path: "level",
                 label: "Level",
                 hdToolTip: "Level",
                 hdClassName: "text-center",
                 sortable: true,
-             },
-             {
+            },
+            {
                 path: "activeFlag",
                 label: "Active",
                 hdToolTip: "Active",
-                hdClassName: "text-center",                
+                hdClassName: "text-center",
                 sortable: true,
-                content:(type)=>this.getTrueFalseElement(type.activeFlag=='Y'),
-                className: "text-center",  
-                          
-             },           
+                content: (type) => this.getTrueFalseElement(type.activeFlag == 'Y'),
+                className: "text-center",
+
+            },
             {
                 path: "edit",
                 label: "",
                 hdToolTip: "Edit",
                 hdClassName: "text-center",
                 icon: "fal fa-2x fa-edit color-gray2 pointer",
-                sortable: false,                
-                className: "text-center",   
-                content:(type)=>this.getEditElement(type,this.showEditModal),             
-             },
-             {
+                sortable: false,
+                className: "text-center",
+                content: (type) => this.getEditElement(type, this.showEditModal),
+            },
+            {
                 path: "trash",
                 label: "",
                 hdToolTip: "Delete",
                 hdClassName: "text-center",
                 icon: "fal fa-2x fa-trash-alt color-gray2 pointer",
-                sortable: false,                
-                className: "text-center",   
-                content:(type)=>this.getDeleteElement(type,this.handleDelete,type.canDelete),             
-             }
+                sortable: false,
+                className: "text-center",
+                content: (type) => this.getDeleteElement(type, this.handleDelete, type.canDelete),
+            }
         ];
-    
-        return <Table           
-        style={{width:700,marginTop:20}}
-        key="leadStatus"
-        pk="teamID"
-        columns={columns}
-        data={this.state.teams||[]}
-        search={true}
+
+        return <Table
+            style={{width: 700, marginTop: 20}}
+            key="leadStatus"
+            pk="teamID"
+            columns={columns}
+            data={this.state.teams || []}
+            search={true}
         >
         </Table>
     }
-    showEditModal=(data)=>{        
-        this.setState({showModal:true,data:{...data},mode:'edit'});
+    showEditModal = (data) => {
+        this.setState({showModal: true, data: {...data}, mode: 'edit'});
     }
-    handleDelete=async (type)=>{
-        const conf=await this.confirm("Are you sure to delete this team?")
-        if(conf)
-        this.api.deleteTeam(type.teamID).then(res=>{
-            if(res.state)
-            this.getData();
-            else this.alert(res.error);
-        })
+    handleDelete = async (type) => {
+        const conf = await this.confirm("Are you sure to delete this team?")
+        if (conf)
+            this.api.deleteTeam(type.teamID).then(res => {
+                if (res.state)
+                    this.getData();
+                else this.alert(res.error);
+            })
     }
-     
-    handleNewType=()=>{
-        this.setState({mode:"new",showModal:true, data:{
-            teamID:'',
-            name:'',            
-        }});
-    }
-    hideModal=()=>{
-        this.setState({ showModal:false});
-    }
-    getModalElement=()=>{
-        const {mode,data,roles,users}=this.state;
-        return <Modal 
-        width={500}
-        show={this.state.showModal}
-        title={mode=="new"?"Add New Type":"Edit Type"}
-        onClose={this.hideModal}
-        content={
-            <div key="content">
 
-                <div className="form-group">
-                  <label >Name</label>
-                  <input value={data.name} type="text" name="" id="" className="form-control required" onChange={(event)=>this.setValue("name",event.target.value)} />                   
+    handleNewType = () => {
+        this.setState({
+            mode: "new", showModal: true, data: {
+                teamID: '',
+                name: '',
+            }
+        });
+    }
+    hideModal = () => {
+        this.setState({showModal: false});
+    }
+    getModalElement = () => {
+        const {mode, data, roles, users} = this.state;
+        return <Modal
+            width={500}
+            show={this.state.showModal}
+            title={mode == "new" ? "Add New Type" : "Edit Type"}
+            onClose={this.hideModal}
+            content={
+                <div key="content">
+
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input value={data.name} type="text" name="" id="" className="form-control required"
+                               onChange={(event) => this.setValue("name", event.target.value)}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Level</label>
+                        <input type="number" value={data.level} name="" id="" className="form-control required"
+                               onChange={(event) => this.setValue("level", event.target.value)}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Role</label>
+                        <select value={data.teamRoleID}
+                                onChange={(event) => this.setValue("teamRoleID", event.target.value)}>
+                            <option/>
+                            {roles.map(r => <option key={r.id} value={r.id}>{r.name}</option>)}
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Leader</label>
+                        <select value={data.leaderId}
+                                onChange={(event) => this.setValue("leaderId", event.target.value)}>
+                            <option/>
+                            {users
+                                .filter(u => u.active || u.id == data.leaderId)
+                                .map(r => <option key={r.id} value={r.id} disabled={!r.active}>{r.name}</option>)
+                            }
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Active</label>
+                        <Toggle checked={data.activeFlag == 'Y'}
+                                onChange={(event) => this.setValue("activeFlag", data.activeFlag == 'Y' ? 'N' : 'Y')}/>
+                    </div>
                 </div>
-                <div className="form-group">
-                  <label >Level</label>
-                  <input type="number" value={data.level}   name="" id="" className="form-control required" onChange={(event)=>this.setValue("level",event.target.value)} />                   
-                </div>
-                <div className="form-group">
-                  <label >Role</label>                  
-                  <select value={data.teamRoleID} onChange={(event)=>this.setValue("teamRoleID",event.target.value)}>
-                    <option></option>
-                    {roles.map(r=><option key={r.id} value={r.id}>{r.name}</option>)}
-                  </select>
-                </div>
-                <div className="form-group">
-                  <label >Leader</label>                  
-                  <select value={data.leaderId} onChange={(event)=>this.setValue("leaderId",event.target.value)}>
-                    <option></option>
-                    {users.map(r=><option key={r.id} value={r.id}>{r.name}</option>)}
-                  </select>
-                </div>
-                <div className="form-group">
-                  <label >Active</label>
-                  <Toggle checked={data.activeFlag=='Y'} onChange={(event)=>this.setValue("activeFlag",data.activeFlag=='Y'?'N':'Y')} ></Toggle>
-                </div>
-            </div>        
-        }
-        footer={<div key="footer">
+            }
+            footer={<div key="footer">
                 <button onClick={this.handleSave}>Save</button>
                 <button onClick={this.hideModal}>Cancel</button>
             </div>}
@@ -193,45 +204,44 @@ class TeamComponent extends MainComponent {
 
         </Modal>
     }
-    handleSave=()=>{
-        const { data, mode } = this.state;
+    handleSave = () => {
+        const {data, mode} = this.state;
         if (data.name == "") {
-          this.alert("Type name required.");
-          return;
-        }        
-        if (mode == "new") {
-          this.api.addTeam(data).then((result) => {
-            if (result.state) {
-              this.setState({ showModal: false });
-             
-            } else {
-              this.alert(result.error);
-            }
-            this.getData();
-          });
+            this.alert("Type name required.");
+            return;
         }
-        else if(mode=='edit')
-        {
+        if (mode == "new") {
+            this.api.addTeam(data).then((result) => {
+                if (result.state) {
+                    this.setState({showModal: false});
+
+                } else {
+                    this.alert(result.error);
+                }
+                this.getData();
+            });
+        } else if (mode == 'edit') {
             this.api.updateTeam(data).then((result) => {
-              if (result.state) {
-                this.setState({ showModal: false });              
-              } else {
-                this.alert(result.error);
-              }
-              this.getData();
+                if (result.state) {
+                    this.setState({showModal: false});
+                } else {
+                    this.alert(result.error);
+                }
+                this.getData();
             });
         }
     }
-    render() {        
+
+    render() {
         return <div>
-            <Spinner show={this.state.showSpinner}></Spinner>
+            <Spinner show={this.state.showSpinner}/>
             <ToolTip title="New Type" width={30}>
-                <i className="fal fa-2x fa-plus color-gray1 pointer" onClick={this.handleNewType}></i>
+                <i className="fal fa-2x fa-plus color-gray1 pointer" onClick={this.handleNewType}/>
             </ToolTip>
             {this.getConfirm()}
             {this.getAlert()}
             {this.getModalElement()}
-           {this.getDataTable()}
+            {this.getDataTable()}
         </div>;
     }
 }
