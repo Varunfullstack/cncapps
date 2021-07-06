a9e0fc2380f437ba84b073bdd17623be40d7f4cc
fizdalf <fizdalf@gmail.com>
2021-07-06 10:04:28 +0100

    Fix issue

--- cnccode/controller_classes/CTContractReport.php
+++ cnccode/controller_classes/CTContractReport.php
@@ -6,7 +6,6 @@
  * @access public
  * @authors Karim Ahmed - Sweet Code Limited
  */
-
 global $cfg;
 require_once($cfg['path_ct'] . '/CTCNC.inc.php');
 
@@ -26,20 +25,20 @@ class CTContractReport extends CTCNC
     {
         switch ($this->getAction()) {
             case 'getData':
-                $draw = $_REQUEST['draw'];
+                $draw    = $_REQUEST['draw'];
                 $columns = $_REQUEST['columns'];
-                $search = $_REQUEST['search'];
-                $order = $_REQUEST['order'];
-                $offset = $_REQUEST['start'];
-                $limit = $_REQUEST['length'];
-
-                $columnsNames = [
+                $search  = $_REQUEST['search'];
+                $order   = $_REQUEST['order'];
+                $offset  = $_REQUEST['start'];
+                $limit   = $_REQUEST['length'];
+                $columnsNames      = [
                     'contractId',
                     'users',
                     'customerName',
                     'siteAddress',
                     'itemDescription',
                     'renewalStatus',
+                    'initialContractLength',
                     'startDate',
                     'expiryDate',
                     'value',
@@ -47,27 +46,25 @@ class CTContractReport extends CTCNC
                     'topUp',
                 ];
                 $columnsDefinition = [
-                    'contractId'      => 'custitem.cui_cuino',
-                    'users'           => 'custitem.cui_users',
-                    'customerName'    => 'customer.cus_name',
-                    'siteAddress'     => 'address.add_add1',
-                    'itemDescription' => 'item.itm_desc',
-                    'renewalStatus'   => 'custitem.renewalStatus',
-                    'startDate'       => 'custitem.cui_desp_date',
-                    'expiryDate'      => 'custitem.cui_expiry_date',
-                    'value'           => 'custitem.cui_sale_price',
-                    'balance'         => 'custitem.curGSCBalance',
-                    'topUp'           => 'customer.gscTopUpAmount',
+                    'contractId'            => 'custitem.cui_cuino',
+                    'users'                 => 'custitem.cui_users',
+                    'customerName'          => 'customer.cus_name',
+                    'siteAddress'           => 'address.add_add1',
+                    'itemDescription'       => 'item.itm_desc',
+                    'renewalStatus'         => 'custitem.renewalStatus',
+                    'initialContractLength' => 'custitem.initialContractLength',
+                    'startDate'             => 'custitem.cui_desp_date',
+                    'expiryDate'            => 'custitem.cui_expiry_date',
+                    'value'                 => 'custitem.cui_sale_price',
+                    'balance'               => 'custitem.curGSCBalance',
+                    'topUp'                 => 'customer.gscTopUpAmount',
                 ];
-
                 $columnsTypes = [
                     'startDate'  => "date",
                     'expiryDate' => "date"
                 ];
-
-                /** @var dbSweetcode $db */
-                global $db;
-                $countQuery = "select count(*) FROM custitem
+                /** @var dbSweetcode $db */ global $db;
+                $countQuery       = "select count(*) FROM custitem
          JOIN customer ON customer.cus_custno = custitem.cui_custno
          JOIN item ON item.itm_itemno = custitem.cui_itemno
          JOIN address ON address.add_siteno = custitem.cui_siteno AND address.add_custno = custitem.cui_custno
@@ -76,8 +73,8 @@ WHERE 1 = 1
   AND item.renewalTypeID IS NOT NULL
   AND item.renewalTypeID <> 0";
                 $totalCountResult = $db->query($countQuery);
-                $totalCount = $totalCountResult->fetch_row()[0];
-                $defaultQuery = "
+                $totalCount       = $totalCountResult->fetch_row()[0];
+                $defaultQuery     = "
 SELECT custitem.cui_cuino                                as 'contractId',
        custitem.cui_users                                as 'users',
        customer.cus_name                                 as 'customerName',
@@ -87,8 +84,9 @@ SELECT custitem.cui_cuino                                as 'contractId',
        custitem.cui_expiry_date                          as 'expiryDate',
        custitem.cui_sale_price                           as 'value',
        custitem.curGSCBalance                            as 'balance',
-       customer.gscTopUpAmount                           as 'topUp',
-       custitem.renewalStatus                            as 'renewalStatus'
+       customer.gscTopUpAmount                           as 'topUp', 
+       custitem.renewalStatus                            as 'renewalStatus',
+       custitem.initialContractLength                    as 'initialContractLength'
 FROM custitem
          JOIN customer ON customer.cus_custno = custitem.cui_custno
          JOIN item ON item.itm_itemno = custitem.cui_itemno
@@ -97,23 +95,22 @@ WHERE 1 = 1
   AND customer.cus_custno <> 2511
   AND item.renewalTypeID IS NOT NULL
   AND item.renewalTypeID <> 0";
-                $columnSearch = [];
-                $parameters = [];
+                $columnSearch     = [];
+                $parameters       = [];
                 foreach ($columns as $column) {
                     if (!isset($columnsDefinition[$column['data']])) {
                         continue;
                     }
-
                     if ($column['search']['value']) {
                         if (!isset($columnsTypes[$column['data']])) {
                             $columnSearch[] = $columnsDefinition[$column['data']] . " like ?";
-                            $parameters[] = [
+                            $parameters[]   = [
                                 "type"  => "s",
                                 "value" => "%" . $column['search']['value'] . "%"
                             ];
                         } elseif ($columnsTypes[$column['data']] == 'date') {
                             $columnSearch[] = $columnsDefinition[$column['data']] . " = ?";
-                            $parameters[] = [
+                            $parameters[]   = [
                                 "type"  => "s",
                                 "value" => $column['search']['value']
                             ];
@@ -122,13 +119,11 @@ WHERE 1 = 1
 
                     }
                 }
-
                 if (count($columnSearch)) {
-                    $wherePart = " and " . implode(" and ", $columnSearch);
+                    $wherePart    = " and " . implode(" and ", $columnSearch);
                     $defaultQuery .= $wherePart;
-                    $countQuery .= $wherePart;
+                    $countQuery   .= $wherePart;
                 }
-
                 $orderBy = [];
                 if (count($order)) {
                     foreach ($order as $orderItem) {
@@ -144,21 +139,19 @@ WHERE 1 = 1
                         $defaultQuery .= (" order by " . implode(' , ', $orderBy));
                     }
                 }
-                $countResult = $db->preparedQuery(
+                $countResult   = $db->preparedQuery(
                     $countQuery,
                     $parameters
                 );
                 $filteredCount = $countResult->fetch_row()[0];
-
                 $defaultQuery .= " limit ?,?";
                 $parameters[] = ["type" => "i", "value" => $offset];
                 $parameters[] = ["type" => "i", "value" => $limit];
-                $result = $db->preparedQuery(
+                $result       = $db->preparedQuery(
                     $defaultQuery,
                     $parameters
                 );
-                $data = $result->fetch_all(MYSQLI_ASSOC);
-
+                $data         = $result->fetch_all(MYSQLI_ASSOC);
                 echo json_encode(
                     [
                         "draw"            => +$draw,
@@ -167,7 +160,6 @@ WHERE 1 = 1
                         "data"            => $data
                     ]
                 );
-
                 break;
             default:
                 $this->displayList();
@@ -200,10 +192,7 @@ WHERE 1 = 1
         );
         global $twig;
         $changelog = $twig->render('@internal/contractReport/contractReport.html.twig');
-
-
         $this->template->set_var('changeLog', $changelog);
-
         $this->template->parse('CONTENTS', 'ChangeLog', true);
         $this->parsePage();
     }
