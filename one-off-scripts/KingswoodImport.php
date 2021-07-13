22853cb16d79352de63c29699ac3ff395c1ac788
fizdalf <fizdalf@gmail.com>
2021-07-13 09:11:35 +0100

    One off script to update kingswood contacts

--- one-off-scripts/KingswoodImport.php
+++ one-off-scripts/KingswoodImport.php
@@ -1,6 +1,7 @@
 <?php
 require_once(__DIR__ . "/../htdocs/config.inc.php");
 global $cfg;
+require_once $cfg['path_dbe'] . '/DBEContact.inc.php';
 $that       = null;
 $dbeContact = new DBEContact($that);
 $dbeContact->getRowsByCustomerID('6391');
@@ -21,16 +22,17 @@ function handleRow($data, dbSweetcode $db, &$existingContacts): void
 {
     $email  = $data[8];
     $toFind = "%$email%";
-    $db->query("select * from contact where con_email like {$toFind} and con_custno = 6391");
+    $db->query("select * from contact where con_email like '{$toFind}' and con_custno = 6391");
     if (!$db->num_rows()) {
         $toInsert = new DBEContact($that);
         $toInsert->setValue(DBEContact::email, $email);
+        $toInsert->setValue(DBEContact::customerID, 6391);
         $toInsert->setValue(DBEContact::title, $data[0]);
         $toInsert->setValue(DBEContact::firstName, $data[1]);
         $toInsert->setValue(DBEContact::lastName, $data[2]);
         $toInsert->setValue(DBEContact::position, $data[3]);
-        $toInsert->setValue(DBEContact::siteNo, $data[4]);
-        $toInsert->setValue(DBEContact::supportLevel, $data[5]);
+        $toInsert->setValue(DBEContact::supportLevel, strtolower($data[4]));
+        $toInsert->setValue(DBEContact::siteNo, $data[5]);
         $toInsert->setValue(DBEContact::phone, $data[6]);
         $toInsert->setValue(DBEContact::mobilePhone, $data[7]);
         $toInsert->setValue(DBEContact::initialLoggingEmailFlag, 'Y');
@@ -44,15 +46,24 @@ function handleRow($data, dbSweetcode $db, &$existingContacts): void
     }
     // update the guy
     $db->next_record(MYSQLI_ASSOC);
-    $existingContacts[$db->Record[DBEContact::contactID]] = true;
-    $toUpdate                                             = new DBEContact($that);
-    $toUpdate->getRow($db->Record[DBEContact::contactID]);
+    $contactId                    = $db->Record['con_contno'];
+    $existingContacts[$contactId] = true;
+    $toUpdate                     = new DBEContact($that);
+    $toUpdate->getRow($contactId);
+    if ($contactId === "21442") {
+        $toUpdate->setValue(DBEContact::accountsFlag, 'Y');
+        $toUpdate->setValue(DBEContact::mailshot2Flag, 'Y');
+        $toUpdate->setValue(DBEContact::mailshot4Flag, 'Y');
+        $toUpdate->setValue(DBEContact::mailshot4Flag, 'Y');
+        $toUpdate->setValue(DBEContact::reviewUser, 'Y');
+        $toUpdate->setValue(DBEContact::mailshot9Flag, 'Y');
+    }
     $toUpdate->setValue(DBEContact::title, $data[0]);
     $toUpdate->setValue(DBEContact::firstName, $data[1]);
     $toUpdate->setValue(DBEContact::lastName, $data[2]);
     $toUpdate->setValue(DBEContact::position, $data[3]);
-    $toUpdate->setValue(DBEContact::siteNo, $data[4]);
-    $toUpdate->setValue(DBEContact::supportLevel, $data[5]);
+    $toUpdate->setValue(DBEContact::supportLevel, strtolower($data[4]));
+    $toUpdate->setValue(DBEContact::siteNo, $data[5]);
     $toUpdate->setValue(DBEContact::phone, $data[6]);
     $toUpdate->setValue(DBEContact::mobilePhone, $data[7]);
     $toUpdate->updateRow();
